c24e55af27bf911479b7bcf79dcd5ffa5336d0f1
Matt Fleming <7wjchnb836@snkmail.com>
2014-07-17 17:50:50 -0500

    Typo in the factory definition

--- app/scripts/vendor/rfc6570.js
+++ app/scripts/vendor/rfc6570.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('emmiManager')
-    .factory('UriTemplate', function ($http) {
+    .factory('UriTemplate', function () {
 
         /* jshint ignore:start */
         var operatorOptions = {
