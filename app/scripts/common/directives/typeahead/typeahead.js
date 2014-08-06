a318891bc41c4a367bb89fcc4bd2c13e0ffc8de3
Evan Black <evanmblack@gmail.com>
2014-08-06 15:57:04 -0500

    Example component for EM-53

---
+++ app/scripts/common/directives/typeahead/typeahead.js
@@ -0,0 +1,220 @@
+'use strict';
+
+angular.module('emmi.typeahead', [])
+    .directive('typeahead', function ($timeout){
+        // Runs during compile
+        return {
+            // name: '',
+            // priority: 1,
+            // terminal: true,
+            // require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
+            restrict: 'AE', // E = Element, A = Attribute, C = Class, M = Comment
+            templateUrl: 'scripts/common/directives/typeahead/typeahead.tpl.html',
+            replace: true,
+            transclude: true,
+            scope: {
+                search: '&',
+                select: '&',
+                expand: '&',
+                items: '=',
+                term: '=',
+                paging: '='
+            },
+            controller: function($scope, $element, $attrs, $transclude) {
+                $scope.items = [];
+                $scope.hide = false;
+
+                this.activate = function(item) {
+                    $scope.active = item;
+                };
+
+                this.activateNextItem = function() {
+                    var index = $scope.items.indexOf($scope.active);
+                    if ((index+1) >= ($scope.paging.currentPage+1)*$scope.paging.itemsPerPage) {
+                        $scope.paging.currentPage++;
+                    }
+                    this.activate($scope.items[(index + 1) % $scope.items.length]);
+                };
+
+                this.activatePreviousItem = function() {
+                    var index = $scope.items.indexOf($scope.active);
+                    if ((index-1) < ($scope.paging.currentPage)*$scope.paging.itemsPerPage) {
+                        $scope.paging.currentPage--;
+                    }
+                    this.activate($scope.items[index === 0 ? $scope.items.length - 1 : index - 1]);
+                };
+
+                this.isActive = function(item) {
+                    return $scope.active === item;
+                };
+
+                this.selectActive = function() {
+                    this.select($scope.active);
+                };
+
+                this.select = function(item) {
+                    $scope.hide = true;
+                    $scope.focused = true;
+                    $scope.select({item:item});
+                };
+
+                $scope.isVisible = function() {
+                    return !$scope.hide && ($scope.focused || $scope.mousedOver);
+                };
+
+                $scope.query = function() {
+                    $scope.hide = false;
+                    $scope.search({term:$scope.term});
+                };
+
+                $scope.all = function() {
+                    $scope.hide = false;
+                    $scope.focused = true;
+                    $scope.expand();
+                };
+
+                $scope.pageCount = function() {
+                    return Math.ceil($scope.items.length/$scope.paging.itemsPerPage)-1;
+                };
+
+            },
+            //link: function($scope, iElm, iAttrs, controller) {
+            link: function(scope, element, attrs, controller) {
+
+                var $input = element.find('form > input');
+                var $list = element.find('> div');
+                var $listMenu = element.find('.menu');
+
+                function debounce(func, wait, immediate) {
+                    var timeout;
+                    return function() {
+                        var context = this, args = arguments;
+                        clearTimeout(timeout);
+                        timeout = setTimeout(function() {
+                            timeout = null;
+                            if (!immediate) {
+                                func.apply(context, args);
+                            }
+                        }, wait);
+                        if (immediate && !timeout) {
+                            func.apply(context, args);
+                        }
+                    };
+                }
+
+                $input.bind('focus', function() {
+                    scope.$apply(function() { scope.focused = true; });
+                });
+
+                $input.bind('blur', function() {
+                    scope.$apply(function() { scope.focused = false; });
+                });
+
+                $list.bind('mouseover', function() {
+                    scope.$apply(function() { scope.mousedOver = true; });
+                });
+
+                $list.bind('mouseleave', function() {
+                    scope.$apply(function() { scope.mousedOver = false; });
+                });
+
+                $listMenu.bind('scroll', debounce(function() {
+                    if ($listMenu.scrollTop() + $listMenu.innerHeight() >= $listMenu.get(0).scrollHeight) {
+                        //console.log('Bottom reached! Paginate!');
+                        if (scope.paging.currentPage < scope.pageCount()) {
+                            $listMenu.get(0).scrollTop = 2;
+                            scope.$apply(function() { scope.paging.currentPage++; });
+                        }
+                    }
+                    if ($listMenu.scrollTop() === 0) {
+                        //console.log('Top reached! Paginate!');
+                        if (scope.paging.currentPage > 0) {
+                            // total height of the list (350) - height of the list viewport (210) = 140
+                            $listMenu.get(0).scrollTop = $listMenu.find('ul').innerHeight() - $listMenu.innerHeight() - 2;
+                            scope.$apply(function() { scope.paging.currentPage--; });
+                        }
+                    }
+                }, 500));
+
+                $input.bind('keyup', function(e) {
+                    if (e.keyCode === 9 || e.keyCode === 13) {
+                        scope.$apply(function() { controller.selectActive(); });
+                    }
+
+                    if (e.keyCode === 27) {
+                        scope.$apply(function() { scope.hide = true; });
+                    }
+                });
+
+                $input.bind('keydown', function(e) {
+                    if (e.keyCode === 9 || e.keyCode === 13 || e.keyCode === 27) {
+                        e.preventDefault();
+                    }
+
+                    if (e.keyCode === 40) {
+                        e.preventDefault();
+                        scope.$apply(function() { controller.activateNextItem(); });
+                    }
+
+                    if (e.keyCode === 38) {
+                        e.preventDefault();
+                        scope.$apply(function() { controller.activatePreviousItem(); });
+                    }
+                });
+
+                scope.$watch('items', function(items) {
+                    controller.activate(items.length ? items[0] : null);
+                });
+
+                scope.$watch('focused', function(focused) {
+                    if (focused) {
+                        $timeout(function() { $input.focus(); }, 0, false);
+                    }
+                });
+
+                scope.$watch('isVisible()', function(visible) {
+                    if (visible) {
+                        var pos = $input.position();
+                        var height = $input[0].offsetHeight;
+
+                        $list.css({
+                            //top: pos.top + height,
+                            //left: pos.left,
+                            //position: 'absolute',
+                            display: 'block'
+                        });
+                    } else {
+                        $list.css('display', 'none');
+                    }
+                });
+
+            }
+        };
+    })
+
+    .directive('typeaheadItem', function() {
+        return {
+            require: '^typeahead',
+            link: function(scope, element, attrs, controller) {
+
+                var item = scope.$eval(attrs.typeaheadItem);
+
+                scope.$watch(function() { return controller.isActive(item); }, function(active) {
+                    if (active) {
+                        element.addClass('active');
+                    } else {
+                        element.removeClass('active');
+                    }
+                });
+
+                element.bind('mouseenter', function(e) {
+                    scope.$apply(function() { controller.activate(item); });
+                });
+
+                element.bind('click', function(e) {
+                    scope.$apply(function() { controller.select(item); });
+                });
+            }
+        };
+    })
+;
