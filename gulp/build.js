7d04f80ccd704023f19c981ee26eeba243d8f2e7
Evan Black <evanmblack@gmail.com>
2014-08-22 10:20:16 -0500

    Modifying gulp build to account for directive templates

--- gulp/build.js
+++ gulp/build.js
@@ -21,7 +21,7 @@ gulp.task('scripts', function () {
 });
 
 gulp.task('partials', function () {
-    return gulp.src('app/partials/**/*.html')
+    return gulp.src(['app/partials/**/*.html', 'app/scripts/**/*.html'])
         .pipe($.minifyHtml({
             empty: true,
             spare: true,
