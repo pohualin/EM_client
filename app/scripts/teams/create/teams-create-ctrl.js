550e1665454276e88fa110bc68e25ced97598a5d
Desalvo, Claudio <Claudio.Desalvo@gmail.com>
2014-09-27 15:52:39 -0300

    EM-186

--- app/scripts/teams/create/teams-create-ctrl.js
+++ app/scripts/teams/create/teams-create-ctrl.js
@@ -1,35 +1,81 @@
 'use strict';
 
 angular.module('emmiManager')
-	/**
-	 * Create a Single Team
-	 */
-    .controller('ClientTeamCreateCtrl',function ($scope,$http, $routeParams, Session, UriTemplate, CreateTeam, ViewTeam, $alert){
+    /**
+     * Create a Single Team
+     */
+    .controller('ClientTeamCreateCtrl',function ($scope,$http, $routeParams, Session, UriTemplate, CreateTeam, ViewTeam, $alert, debounce){
         $scope.team = {
-	        'name': null,
-	        'description': null,
-	        'active': true,
-	        'phone': null,
-	        'fax': null,
-            'client': {
-            	'id':null
+            'name': null,
+            'description': null,
+            'active': true,
+            'phone': null,
+            'fax': null,
+            'client': { 
+                'id':null
             }
-	    };        
-        
+        };        
+
         $scope.team.client.id = $routeParams.clientId;
         $scope.save = function (isValid) {
-        	$scope.formSubmitted = true;
-        	if(isValid){        		        		
+            $scope.formSubmitted = true;
+            if(isValid){                                
                 CreateTeam.insertTeams($scope.team).then(function (team) {
-                	$scope.team = team.data.entity;
+                    $scope.team = team.data.entity;
                     ViewTeam.viewTeam($scope.team);
                 });
-        	}
-        	else {
+            }
+            else {
                 $scope.showError();
             }
         };
-        
+
+        $scope.sfSearch = {};
+        $scope.sfResult = {};
+        $scope.sfResult.account = [];
+        $scope.formSubmitted = false;
+
+        CreateTeam.getReferenceData().then(function (refData) {
+            $scope.findTeamSalesForceAccountLink = refData.link.findTeamSalesForceAccount;
+            $scope.findSalesForceAccount = function () {
+                CreateTeam.findSalesForceAccount(refData.link.findTeamSalesForceAccount, $scope.sfSearch.searchQuery).then(function (searchResults) {
+                    if (searchResults.entity) {
+                        $scope.sfResult = searchResults.entity;
+                    } else {
+                        $scope.sfResult = null;
+                    }
+                });
+            };
+        });
+
+        $scope.findAccount = debounce(function (term) {
+            CreateTeam.findSalesForceAccount($scope.findTeamSalesForceAccountLink, term).then(function (searchResults) {
+                if (searchResults.entity) {
+                    $scope.sfResult = searchResults.entity;
+                } else {
+                    // No results returned
+                    $scope.sfResult = {};
+                    $scope.sfResult.account = [];
+                }
+            });
+        }, 333);
+
+        $scope.chooseAccount = function (account) {
+            if (account ) {
+                $scope.team.teamSalesForceAccount = account;
+                return true;
+            } else {
+                return false;
+            }
+        };
+
+       $scope.changeSfAccount = function () {
+            $scope.sfSearch.searchQuery = $scope.team.teamSalesForceAccount.name;
+            $scope.sfResult.account = [];
+            $scope.team.teamSalesForceAccount = null;
+            focus('SfSearch');
+        };
+
         $scope.showError = function(){
             if (!$scope.errorAlert) {
                 $scope.errorAlert = $alert({
--- app/scripts/teams/create/teams-create-service.js
+++ app/scripts/teams/create/teams-create-service.js
@@ -1,13 +1,32 @@
 'use strict';
 angular.module('emmiManager')
     .service('CreateTeam', function ($http, $q, Session, UriTemplate, $location) {
-    	var selectedTeam;
-    	return {
-    	    insertTeams: function (team) {
+      var selectedTeam;
+      var referenceData;
+      return {
+          insertTeams: function (team) {
                return $http.post(UriTemplate.create(Session.link.teamsByClientId).stringify({clientId: team.client.id}), team).
                    then(function (response) {
-            	       return response;
+                     return response;
                    });
-    	    }
-    	};
-}); 	 
+          },
+            getReferenceData: function () {
+                var deferred = $q.defer();
+                if (!referenceData) {
+                    $http.get(Session.link.clientsReferenceData).then(function (response) {
+                        referenceData = response.data;
+                        deferred.resolve(referenceData);
+                    });
+                } else {
+                    deferred.resolve(referenceData);
+                }
+                return deferred.promise;
+            },
+            findSalesForceAccount: function (href, searchString) {
+                return $http.get(UriTemplate.create(href).stringify({q: searchString}))
+                    .then(function (response) {
+                        return response.data;
+                    });
+            }                             
+      };
+});    
