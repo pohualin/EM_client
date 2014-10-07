f9e07f1866630e78c93d7d83309d98d544ebc20b
Desalvo, Claudio <Claudio.Desalvo@cmegroup.com>
2014-10-07 13:06:34 -0300

    EM-256

--- app/scripts/teams/locations/teams-locations-ctrl.js
+++ app/scripts/teams/locations/teams-locations-ctrl.js
@@ -5,14 +5,15 @@ angular.module('emmiManager')
     .controller('TeamsLocationsController', function ($scope, TeamLocation, Location, $http, Session, UriTemplate, $controller, Client, $modal) {
 
         TeamLocation.getReferenceData().then(function (refData) {
-            $scope.clientsWithLocationsLnk = refData.link.clientWithLocations;
-            TeamLocation.findClientsWithLocations($scope.clientsWithLocationsLnk).then(function (refData) {
-                $scope.clientsWithLocations = refData;
-            });            
+            $scope.clientsWithLocationsLnk = refData.link.clientWithLocations;       
+        });
+
+        Location.findForClient(Client.getClient()).then(function (allLocations) {
+            $scope.clientLocations = allLocations;
         });
 
         $scope.clientHasLocations = function () {
-            return $scope.clientsWithLocations.length > 0;      
+            return $scope.clientLocations.length > 0;      
         };
 
         var addNewLocationsModal = $modal({scope: $scope, template: 'partials/team/locations/search.html', animation: 'none', backdropAnimation: 'emmi-fade', show: false});
@@ -23,6 +24,18 @@ angular.module('emmiManager')
 
         $scope.hideAddLocationsModal = function () {
             $scope.$hide();
+        };
+
+        $scope.search = function () {
+            $scope.changedLocations = {};
+            $scope.loading = true;
+            Location.find($scope.locationQuery, $scope.status).then(function (locationPage) {
+                $scope.locations = locationPage.content ;
+                $scope.loading = false;
+            }, function () {
+                // error happened
+                $scope.loading = false;
+            });
         };
 
     })
