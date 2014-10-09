4e26de8b835ccc1487553dde3a0137b697d4e705
Desalvo, Claudio <Claudio.Desalvo@cmegroup.com>
2014-10-09 10:13:36 -0300

    EM-256: multiselect

--- app/scripts/teams/locations/teams-locations-ctrl.js
+++ app/scripts/teams/locations/teams-locations-ctrl.js
@@ -4,25 +4,44 @@ angular.module('emmiManager')
 
     .controller('TeamsLocationsController', function ($scope, TeamLocation, Location, $http, Session, UriTemplate, $controller, Client, $modal) {
 
-        TeamLocation.getReferenceData().then(function (refData) {
-            $scope.clientsWithLocationsLnk = refData.link.clientWithLocations;       
-        });
+        $scope.teamLocations = {};
+        $scope.teamLocationsArray = [];
 
-        Location.findForClient(Client.getClient()).then(function (allLocations) {
-            $scope.clientLocations = allLocations;
-        });
-
-        $scope.clientHasLocations = function () {
-            return $scope.clientLocations.length > 0;      
-        };
-
-        var addNewLocationsModal = $modal({scope: $scope, template: 'partials/team/locations/search.html', animation: 'none', backdropAnimation: 'emmi-fade', show: false});
 
         $scope.addLocations = function () {
             addNewLocationsModal.$promise.then(addNewLocationsModal.show);
         };
 
-        $scope.hideAddLocationsModal = function () {
+        Location.findForClient(Client.getClient()).then(function (allLocations) {
+            $scope.clientLocations = allLocations;
+        });
+
+        $scope.teamHasLocations = function () {
+
+            return $scope.teamLocationsArray && $scope.teamLocationsArray.length > 0;  
+            //return false;    
+        };
+
+        $scope.save = function (addAnother) {
+            $scope.teamLocationsArray = [];
+            angular.forEach( $scope.teamLocations , function (location) {
+                $scope.teamLocationsArray.push(location);
+            });
+
+            addNewLocationsModal.$promise.then(addNewLocationsModal.hide);
+        };
+
+        var addNewLocationsModal = $modal({scope: $scope, template: 'partials/team/locations/search.html', animation: 'none', backdropAnimation: 'emmi-fade', show: false});
+
+    })
+
+    .controller('SearchTeamsLocationsController', function ($scope, Location, $http) {
+
+        $scope.clientHasLocations = function () {
+            return $scope.clientLocations.length > 0;      
+        };
+
+        $scope.cancel = function () {
             $scope.$hide();
         };
 
@@ -38,11 +57,20 @@ angular.module('emmiManager')
             });
         };
 
-        $scope.save = function (addAnother) {
-
-            $scope.$hide();
+        /**
+         * Called when the checkbox on the select popup is checked or unchecked
+         * @param locationResource it was checked on
+         */
+        $scope.onCheckboxChange = function (locationResource) {
+            if (locationResource.entity.currentNewLocationState === locationResource.entity.newlocation) {
+                // the checkbox is the same as it was at the start, location not changed
+                delete $scope.teamLocations[locationResource.entity.id];
+            } else {
+                locationResource.entity.currentNewLocationState = true;
+                // this is a change from the saved state, store a copy of the object
+                $scope.teamLocations[locationResource.entity.id] = angular.copy(locationResource);
+            }
         };
-
     })
 
 ;
