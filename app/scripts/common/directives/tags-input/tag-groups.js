70c2070435050857c0bfe36666879189e775d708
Evan Black <evanmblack@gmail.com>
2014-10-13 13:31:05 -0500

    EM-217 revision

--- app/scripts/common/directives/tags-input/tag-groups.js
+++ app/scripts/common/directives/tags-input/tag-groups.js
@@ -125,11 +125,13 @@ angular.module('emmiManager')
                     var unique = {};
                     var dupes = [];
                     angular.forEach($scope.groups, function (x, i) {
-                        var groupTitle = x.title.toLowerCase().replace(/[^a-z0-9]+/g, '');
-                        if (!unique[groupTitle]) {
-                            unique[groupTitle] = true;
-                        } else {
-                            dupes.push(i);
+                        if (x.title) {
+                            var groupTitle = x.title.toLowerCase().replace(/[^a-z0-9]+/g, '');
+                            if (!unique[groupTitle]) {
+                                unique[groupTitle] = true;
+                            } else {
+                                dupes.push(i);
+                            }
                         }
                     });
                     return dupes;
