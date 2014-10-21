a7f147e3ecf78f7bfc3ba8861a6baf7266a7e6c9
Matt Fleming <7wjchnb836@snkmail.com>
2014-10-21 13:02:57 -0500

    EM-356 An emmimanager user can create a new client

---
+++ app/scripts/clients/create/clients-create-ctrl.js
@@ -0,0 +1,46 @@
+'use strict';
+
+angular.module('emmiManager')
+
+/**
+ * Create new controller
+ */
+.controller('ClientCreateController', function ($scope, Client, $controller, Location, Tag, $q) {
+
+    $controller('ViewEditCommon', {$scope: $scope});
+
+    $scope.client = Client.newClient().entity;
+
+    $scope.saveUpdate = function (isValid) {
+        // this will get called if the client form saves but any child calls fail
+        $scope.formSubmitted = true;
+        if (isValid && $scope.client.salesForceAccount) {
+            Client.updateClient($scope.client).then(function () {
+                // update locations for the client
+                Location.updateForClient(Client.getClient()).then(function () {
+                    Client.viewClient($scope.client);
+                });
+            });
+        } else {
+            $scope.showError();
+        }
+    };
+
+    $scope.cancel = function () {
+        Client.viewClientList();
+    };
+
+    $scope.save = function (isValid) {
+        $scope.formSubmitted = true;
+        if (isValid && $scope.client.salesForceAccount) {
+            Client.insertClient($scope.client).then(function (response) {
+                Client.editClient(response.data.entity);
+            });
+        } else {
+            $scope.showError();
+        }
+    };
+
+})
+
+;
