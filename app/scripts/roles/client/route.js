ada24fa371c79ad78da62d84a95732080a572cd9
Matt Fleming <7wjchnb836@snkmail.com>
2014-11-22 02:08:12 -0600

    EM-452 Full CRUD established

--- app/scripts/roles/client/route.js
+++ app/scripts/roles/client/route.js
@@ -3,23 +3,28 @@
 angular.module('emmiManager')
     .config(function ($routeProvider, USER_ROLES) {
 
-        var requiredResources = {
-            'account': ['AuthSharedService', function (AuthSharedService) {
-                return AuthSharedService.currentUser();
+        var clientDetailRequiredResources = {
+            'clientResource': ['AuthSharedService','Client', '$route', '$q', function (AuthSharedService, Client, $route, $q){
+                var deferred = $q.defer();
+                AuthSharedService.currentUser().then(function (){
+                    Client.selectClient($route.current.params.clientId).then(function (clientResource){
+                        deferred.resolve(clientResource);
+                    });
+                });
+                return deferred.promise;
             }]
         };
 
         // Routes
         $routeProvider
-            .when('/roles/client', {
+            .when('/clients/:clientId/roles', {
                 templateUrl: 'partials/role/client/main.html',
-                controller: 'ClientRoleAdminMainCtrl',
+                controller: 'ManageClientRolesMainCtrl',
                 access: {
                     authorizedRoles: [USER_ROLES.admin]
                 },
                 reloadOnSearch: false,
-                title: 'Client Role Administration',
-                resolve: requiredResources
+                resolve: clientDetailRequiredResources
             });
     })
 
---
+++ app/scripts/roles/client/service.js
@@ -0,0 +1,83 @@
+'use strict';
+angular.module('emmiManager')
+
+    .service('ManageUserRolesService', ['$filter', '$q', '$http', 'UriTemplate', 'CommonService', 'Client',
+        function ($filter, $q, $http, UriTemplate, CommonService, Client) {
+            var referenceData;
+            return {
+                loadClientRoles: function () {
+                    var deferred = $q.defer();
+                    var roles = [];
+                    $http.get(UriTemplate.create(Client.getClient().link.roles).stringify())
+                        .then(function load(response) {
+                            var page = response.data;
+                            CommonService.convertPageContentLinks(page);
+                            angular.forEach(page.content, function (content) {
+                                content.entity.userClientPermissions = (referenceData) ? angular.copy(referenceData.permission) : [];
+                            });
+                            roles.push.apply(roles, page.content);
+                            if (page.link && page.link['page-next']) {
+                                $http.get(page.link['page-next']).then(function (response) {
+                                    load(response);
+                                });
+                            }
+                            deferred.resolve(roles);
+                        });
+                    return deferred.promise;
+                },
+                loadPermissions: function (roleResource) {
+                    return $http.get(UriTemplate.create(roleResource.link.permissions).stringify())
+                        .then(function (response) {
+                            roleResource.savedPermissions = response.data;
+                            return response.data;
+                        });
+                },
+                saveNewClientRole: function (role) {
+                    var active = $filter('filter')(role.userClientPermissions, {active: true}, true);
+                    return $http.post(UriTemplate.create(Client.getClient().link.roles).stringify(), {
+                        name: role.name,
+                        userClientPermissions: active
+                    }).then(function (response) {
+                        return response.data;
+                    });
+                },
+                saveExistingClientRole: function (clientRoleResource) {
+                    var role = clientRoleResource.entity,
+                        active = $filter('filter')(role.userClientPermissions, {active: true}, true);
+                    return $http.put(UriTemplate.create(clientRoleResource.link.self).stringify(), {
+                        id: role.id,
+                        version: role.version,
+                        client: role.client,
+                        name: role.name,
+                        userClientPermissions: active
+                    }).then(function (response) {
+                        response.data.entity.userClientPermissions = (referenceData) ? angular.copy(referenceData.permission) : [];
+                        return response.data;
+                    });
+                },
+                deleteExistingClientRole: function (clientRoleResource) {
+                    return $http.delete(UriTemplate.create(clientRoleResource.link.self).stringify()).then(function (response) {
+                        return response.data;
+                    });
+                },
+                referenceData: function () {
+                    var deferred = $q.defer();
+                    if (!referenceData) {
+                        $http.get((Client.getClient().link.rolesReferenceData)).then(function (response) {
+                            referenceData = response.data;
+                            deferred.resolve(referenceData);
+                        });
+                    } else {
+                        deferred.resolve(referenceData);
+                    }
+                    return deferred.promise;
+                },
+                newClientRole: function () {
+                    return {
+                        name: '',
+                        userClientPermissions: (referenceData) ? angular.copy(referenceData.permission) : []
+                    }
+                }
+            }
+        }])
+;
