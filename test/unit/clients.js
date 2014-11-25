17792e9c65a3b2688aa1d688c59c2a6e56986ec9
Evan Black <evanmblack@gmail.com>
2014-11-25 17:15:56 -0600

    Fix the client controller test with correct Session and mock data

--- test/unit/clients.js
+++ test/unit/clients.js
@@ -1,32 +1,55 @@
 'use strict';
 
 describe('Client controllers', function(){
-  var scope, ctrl, $httpBackend;
+  var scope, ctrl, $httpBackend, Session;
 
   var api = {};
-  api.messages = '/webapi/messages';
+  api.messages = '/webapi/messages?lang=en';
   api.authenticate = '/webapi/authenticate';
   angular.module('emmiManager.api', []).constant('API', api);
 
   beforeEach(module('emmiManager'));
 
-  beforeEach(inject(function($rootScope, $controller, _$httpBackend_) {
+  beforeEach(inject(function($rootScope, $controller, _$httpBackend_, _Session_) {
+    Session = _Session_;
+    Session.create(
+      "super_admin",
+      "System",
+      "Administrator",
+      null,
+      ["PERM_GOD"],
+      {
+          clientById: "/web-rest/webapi/clients/{id}",
+          clients: "/web-rest/webapi/clients{?page,size,sort,name,status}",
+          clientsReferenceData: "/web-rest/webapi/clients/ref",
+          locationReferenceData: "/web-rest/webapi/locations/ref",
+          locations: "/web-rest/webapi/locations{?page,size,sort,name,status}",
+          providers: "/web-rest/webapi/providers{?page,size,sort,name,status}",
+          providersReferenceData: "/web-rest/webapi/providersReferenceData",
+          refDataGroups: "/web-rest/webapi/referenceGroups{?page,size,sort}",
+          self: "/web-rest/webapi/authenticated",
+          teams: "/web-rest/webapi/teams{?page,size,sort,name,status}",
+          teamsByClientId: "/web-rest/webapi/clients/{clientId}/teams",
+          teamsReferenceData: "/web-rest/webapi/teams/ref"
+      }
+    );
     $httpBackend = _$httpBackend_;
+    $httpBackend.expectGET(api.messages).respond({});
     $httpBackend.expectGET('/web-rest/webapi/clients/ref').
           respond(
             {
               "link" : [ {
                 "rel" : "potentialOwners",
-                "href" : "https://dev6.emmisolutions.com/web-rest/webapi/clients/ref/potentialOwners{?page,size,sort}"
+                "href" : "/web-rest/webapi/clients/ref/potentialOwners{?page,size,sort}"
               }, {
                 "rel" : "findSalesForceAccount",
-                "href" : "https://dev6.emmisolutions.com/web-rest/webapi/sf/find{?q}"
+                "href" : "/web-rest/webapi/sf/find{?q}"
               }, {
                 "rel" : "findByNormalizedName",
-                "href" : "https://dev6.emmisolutions.com/web-rest/webapi/clients/ref/findByNormalizedName{?normalizedName}"
+                "href" : "/web-rest/webapi/clients/ref/findByNormalizedName{?normalizedName}"
               }, {
                 "rel" : "refDataGroups",
-                "href" : "https://dev6.emmisolutions.com/web-rest/webapi/referenceGroups{?page,size,sort}"
+                "href" : "/web-rest/webapi/referenceGroups{?page,size,sort}"
               } ],
               "clientType" : [ {
                 "id" : 1,
@@ -74,6 +97,42 @@ describe('Client controllers', function(){
                 "typeKey" : "FOUR"
               } ],
               "statusFilter" : [ "ALL", "ACTIVE_ONLY", "INACTIVE_ONLY" ]
+            }
+          );
+    $httpBackend.expectGET('/web-rest/webapi/clients/ref/potentialOwners').
+          respond(
+            {
+              "sort" : [ {
+                "property" : "firstName",
+                "direction" : "ASC"
+              } ],
+              "page" : {
+                "size" : 100,
+                "totalElements" : 2,
+                "totalPages" : 1,
+                "number" : 0
+              },
+              "link" : [ {
+                "rel" : "self",
+                "href" : "/web-rest/webapi/clients/ref/potentialOwners{?page,size,sort}"
+              } ],
+              "content" : [ {
+                "link" : [ ],
+                "id" : 3,
+                "version" : 0,
+                "login" : "contract_owner",
+                "firstName" : "Contract",
+                "lastName" : "Owner I",
+                "email" : "contract_owner@emmis0lutions.com"
+              }, {
+                "link" : [ ],
+                "id" : 4,
+                "version" : 0,
+                "login" : "contract_owner2",
+                "firstName" : "Contract",
+                "lastName" : "Owner II",
+                "email" : "contract_owner2@emmis0lutions.com"
+              } ]
             }
           );
     scope = $rootScope.$new();
