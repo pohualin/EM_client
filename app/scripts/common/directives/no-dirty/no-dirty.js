6eeb47d6e99109ba6ce452d4bdbbe52a4c513b52
Matt Fleming <7wjchnb836@snkmail.com>
2014-12-22 15:42:54 -0600

    EM-521 A user admin can receive success messages when adding/editing/deactivating users

---
+++ app/scripts/common/directives/no-dirty/no-dirty.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/**
+ * Allows for input elements to not 'dirty' the form when they are changed.
+ * For example, let's say you have a checkbox on the form but changing
+ * that checkbox state should not make the form dirty (meaning it has modifications).
+ * You would simply make the element like so:
+ * <input type="checkbox" name="foo" ng-model="x.foo" no-dirty-check>
+ */
+angular.module('emmi.noDirtyCheck', [])
+    .directive('noDirtyCheck', function () {
+        return {
+            restrict: 'A',
+            require: 'ngModel',
+            link: function (scope, elm, attrs, ctrl) {
+                ctrl.$pristine = false;
+            }
+        }
+    });
