6e7ecfe7c4fac87b20ac9f0565f3cce895c33858
Wendy Lee <wlee@emmisolutions.com>
2015-03-05 17:12:09 -0600

    Fix for EM-919, UI/UX fixes from story EM-297

--- app/client-facing/secretQuestions/secret-question-ctrl.js
+++ app/client-facing/secretQuestions/secret-question-ctrl.js
@@ -19,6 +19,18 @@ angular.module('emmiManager')
         	if(valid){
         	    SecretQuestionService.saveOrUpdateSecretQuestionResponse($scope.question1.entity);
 	        	SecretQuestionService.saveOrUpdateSecretQuestionResponse($scope.question2.entity);
+	        	var message = 'The secret questions and responses have been successfully save.';
+      
+        		$alert({
+					title: ' ',
+					content: 'The secret questions and responses have been successfully save.',
+					container: 'body',
+					type: 'success',
+					placement: 'top',
+				    show: true,
+				    duration: 5,
+				    dismissable: true
+				});
 	        	$location.path('/');
     		} else {
                 if (!$scope.errorAlert) {
