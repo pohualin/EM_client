188d5d48f739fb1e0c34b9a28f2d0e3d4536959b
Kevin Twery <ktwery@emmisolutions.com>
2015-03-05 09:56:19 -0600

    EM-575

--- app/client-facing/validateEmail/validate-email-ctrl.js
+++ app/client-facing/validateEmail/validate-email-ctrl.js
@@ -24,7 +24,7 @@ angular.module('emmiManager')
 
 
             $scope.notNow = function () {
-                ValidationService.notNow();
+                $location.path(locationBeforeLogin).replace();
             };
         }])
 ;
