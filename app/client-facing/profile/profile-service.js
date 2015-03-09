f294f23f4b5be39ee5611fc9028c9fb7a54a5535
Amrita Chavan <achavan@emmisolutions.com>
2015-03-09 16:43:26 -0500

    EM-767: changed link

--- app/client-facing/profile/profile-service.js
+++ app/client-facing/profile/profile-service.js
@@ -4,12 +4,12 @@ angular.module('emmiManager')
 .service('ProfileService', ['API', '$http', 'UriTemplate', function (api, $http, UriTemplate) {
 	return {
 		update: function (userClient) {
-			return $http.put(UriTemplate.create(api.authenticated).stringify(), userClient).then(function(response) {
+			return $http.put(UriTemplate.create(userClient.link.self).stringify(), userClient).then(function(response) {
 				return response.data;
 			});
 		},
 		get: function (userClient) {
-			return $http.get(userClient.link.getById).then(function(response){
+			return $http.get(userClient.link.self).then(function(response){
 				return response.data;
 			});
 		}
