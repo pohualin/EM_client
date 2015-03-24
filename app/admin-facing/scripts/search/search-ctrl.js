84ce1b50940047b8ced3c10dd18448c9970a0bd3
Matt Fleming <mfleming@emmisolutions.com>
2015-03-24 17:59:53 -0500

    EM-939 A client user can select a program
    first pass

--- app/admin-facing/scripts/search/search-ctrl.js
+++ app/admin-facing/scripts/search/search-ctrl.js
@@ -18,7 +18,36 @@ angular.module('emmiManager')
 
     })
 
+/**
+ * Sorting helper functions
+ */
+    .controller('CommonSort', ['$scope', function ($scope) {
+        $scope.createSortProperty = function (property) {
+            var sort = $scope.sortProperty || {};
+            if (sort && sort.property === property) {
+                // same property was clicked
+                if (!sort.ascending) {
+                    // third click removes sort
+                    sort = null;
+                } else {
+                    // switch to descending
+                    sort.ascending = false;
+                }
+            } else {
+                // change sort property
+                sort.property = property;
+                sort.ascending = true;
+            }
+            return sort;
+        };
+    }])
+
+/**
+ * Pagination helper functions
+ */
     .controller('CommonPagination', ['$scope', function($scope){
+
+        $scope.pageSizes = [5, 10, 15, 25];
 
         $scope.isEmpty = function (obj) {
             if (!obj){
@@ -99,6 +128,8 @@ angular.module('emmiManager')
 
         $controller('CommonPagination', {$scope: $scope});
 
+                $controller('CommonSort', {$scope: $scope});
+
         // set the proper value in the search chooser based upon the path
         if ($location.path() === '/clients'){
             $scope.option = 'Clients';
@@ -111,8 +142,6 @@ angular.module('emmiManager')
         } else if ($location.path() === '/users'){
             $scope.option = 'Users';
         }
-
-        $scope.pageSizes = [5, 10, 15, 25];
 
         // when first loading the page
         var searchObject = $location.search();
@@ -174,25 +203,6 @@ angular.module('emmiManager')
 
             $location.search(queryObject).replace();
             $rootScope.currentRouteQueryString = arrays.toQueryString($location.search());
-        };
-
-        $scope.createSortProperty = function (property){
-            var sort = $scope.sortProperty || {};
-            if (sort && sort.property === property) {
-                // same property was clicked
-                if (!sort.ascending) {
-                    // third click removes sort
-                    sort = null;
-                } else {
-                    // switch to descending
-                    sort.ascending = false;
-                }
-            } else {
-                // change sort property
-                sort.property = property;
-                sort.ascending = true;
-            }
-            return sort;
         };
 
     }])
--- app/client-facing/schedule/schedule-service.js
+++ app/client-facing/schedule/schedule-service.js
@@ -6,11 +6,50 @@ angular.module('emmiManager')
         function ($http, UriTemplate) {
             return {
 
+                /**
+                 * Loads the team from a client
+                 *
+                 * @param clientResource the client on which the team exists
+                 * @param teamId to load
+                 * @returns {*}
+                 */
                 loadTeam: function (clientResource, teamId) {
                     return $http.get(UriTemplate.create(clientResource.link.team).stringify({teamId: teamId}))
                         .success(function (response) {
                             return response.data;
                         });
+                },
+
+                /**
+                 * Finds Programs
+                 *
+                 * @param teamResource for this team
+                 * @param sort order
+                 * @param pageSize how many per page
+                 * @returns {*}
+                 */
+                findPrograms: function (teamResource, sort, pageSize) {
+                    return $http.get(UriTemplate.create(teamResource.link.programs).stringify({
+                            sort: sort && sort.property ? sort.property + ',' + (sort.ascending ? 'asc' : 'desc') : '',
+                            size: pageSize
+                        }
+                    )).then(function (response) {
+                        return response.data;
+                    });
+                },
+
+                /**
+                 * Fetches the next page of programs
+                 *
+                 * @param href to use
+                 * @returns {*}
+                 */
+                fetchProgramPage: function (href) {
+                    return $http.get(href)
+                        .then(function (response) {
+                            return response.data;
+                        });
+
                 }
             };
         }])
