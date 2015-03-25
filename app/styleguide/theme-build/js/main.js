025280d914b7802a0a7e1bd65477e91755c3b5bf
Evan Black <evanmblack@gmail.com>
2015-03-25 16:34:55 -0500

    Typeahead and search components designs

--- app/styleguide/theme-build/js/main.js
+++ app/styleguide/theme-build/js/main.js
@@ -19,9 +19,12 @@ var jQuery_no_conflict = $.noConflict(true);
       "title": "This thing already exists...",
       "content": "The quick, brown fox jumps over a lazy dog. DJs flock by when MTV ax quiz prog."
     };
-    // For Typeahead example
+    // For Typeahead examples
     $scope.selectedState = "";
     $scope.states = ["Alabama","Alaska","Arizona","Arkansas","California","Colorado","Connecticut","Delaware","Florida","Georgia","Hawaii","Idaho","Illinois","Indiana","Iowa","Kansas","Kentucky","Louisiana","Maine","Maryland","Massachusetts","Michigan","Minnesota","Mississippi","Missouri","Montana","Nebraska","Nevada","New Hampshire","New Jersey","New Mexico","New York","North Dakota","North Carolina","Ohio","Oklahoma","Oregon","Pennsylvania","Rhode Island","South Carolina","South Dakota","Tennessee","Texas","Utah","Vermont","Virginia","Washington","West Virginia","Wisconsin","Wyoming"];
+    // For Typeahead examples
+    $scope.selectedName = "";
+    $scope.names = ["Alabama","Alaska","Arizona","Arkansas","California","Colorado","Connecticut","Delaware","Florida","Georgia","Hawaii","Idaho","Illinois","Indiana","Iowa","Kansas","Kentucky","Louisiana","Maine","Maryland","Massachusetts","Michigan","Minnesota","Mississippi","Missouri","Montana","Nebraska","Nevada","New Hampshire","New Jersey","New Mexico","New York","North Dakota","North Carolina","Ohio","Oklahoma","Oregon","Pennsylvania","Rhode Island","South Carolina","South Dakota","Tennessee","Texas","Utah","Vermont","Virginia","Washington","West Virginia","Wisconsin","Wyoming"];
   });
 
 })();
