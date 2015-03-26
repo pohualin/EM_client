5e58ad22ecb6a44c06829460f58b2a032af48565
Matt Fleming <mfleming@emmisolutions.com>
2015-03-26 16:00:56 -0500

    EM-939 A client user can select a program
    First pass

---
+++ app/client-facing/schedule/program/add-program-ctrl.js
@@ -0,0 +1,108 @@
+'use strict';
+
+angular.module('emmiManager')
+    .controller('AddProgramController', ['$scope', '$controller', 'AddProgramService', 'moment', '$alert',
+        function ($scope, $controller, AddProgramService, moment, $alert) {
+
+            // add common pagination and sorting functions
+            $controller('CommonPagination', {$scope: $scope});
+            $controller('CommonSort', {$scope: $scope});
+
+            var contentProperty = 'programs';
+
+            $scope.specialties = null;
+            $scope.providers = null;
+            $scope.locations = null;
+
+            $scope.scheduledProgram = {
+                provider: '',
+                location: '',
+                program: '',
+                viewByDate: moment().add(30, 'days').format('YYYY-MM-DD')
+            };
+
+            $scope.saveScheduledProgram = function(addProgramForm){
+                $scope.addProgramFormSubmitted = true;
+                if ($scope.scheduledProgram.program && addProgramForm.$valid){
+
+                } else {
+                    $scope.showError();
+                }
+            };
+
+            $scope.showAllResults = function (show) {
+                $scope.showAll = show;
+                if (!show) {
+                    performSearch();
+                }
+            };
+
+            $scope.showError = function () {
+                if (!$scope.errorAlert) {
+                    $scope.errorAlert = $alert({
+                        title: ' ',
+                        content: 'Please correct the below information.',
+                        container: '#add-program-alerts-container',
+                        type: 'danger',
+                        show: true,
+                        dismissable: false
+                    });
+                } else {
+                    $scope.errorAlert.show();
+                }
+            };
+
+            $scope.selectProgram = function (programResource) {
+                if (programResource.selected) {
+                    if ($scope.scheduledProgram.program) {
+                        $scope.scheduledProgram.program.selected = false;
+                    }
+                    $scope.scheduledProgram.program = programResource;
+                } else {
+                    $scope.scheduledProgram.program = '';
+                }
+            };
+
+            /**
+             * The actual 'search' function
+             *
+             * @param sort the sort component
+             * @param size the size of the page
+             */
+            var performSearch = function (sort, size) {
+                AddProgramService.findPrograms($scope.team, sort, size).then(function (programPage) {
+                    $scope.handleResponse(programPage, contentProperty);
+                });
+                // turn off the sort after the search request has been made, the response will rebuild
+                $scope.sortProperty = null;
+                $scope.scheduledProgram.program = '';
+            };
+
+            /**
+             * Search again sorting based upon the property
+             *
+             * @param property to sort on
+             */
+            $scope.sort = function (property) {
+                var sort = $scope.createSortProperty(property);
+                performSearch(sort, $scope.currentPageSize);
+            };
+
+            /**
+             * Fetches a page from a pagination link
+             *
+             * @param href to use for the data call
+             */
+            $scope.fetchPage = function (href) {
+                $scope.loading = true;
+                AddProgramService.fetchProgramPage(href).then(function (programPage) {
+                    $scope.handleResponse(programPage, contentProperty);
+                });
+                $scope.scheduledProgram.program = '';
+            };
+
+            // load the programs
+            performSearch();
+        }
+    ])
+;
