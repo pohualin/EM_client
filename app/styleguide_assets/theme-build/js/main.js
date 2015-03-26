8ea4a107a69819bbd4b0c263621ad009c286961f
Evan Black <evanmblack@gmail.com>
2015-03-26 10:04:58 -0500

    Designs for accordion elements

--- app/styleguide_assets/theme-build/js/main.js
+++ app/styleguide_assets/theme-build/js/main.js
@@ -22,17 +22,41 @@ var jQuery_no_conflict = $.noConflict(true);
   ]);
 
   app.controller('MainController', function($scope) {
+
     // For Popover example
     $scope.popover = {
       "title": "This thing already exists...",
       "content": "The quick, brown fox jumps over a lazy dog. DJs flock by when MTV ax quiz prog."
     };
+
     // For Typeahead examples
     $scope.selectedState = "";
     $scope.states = ["Alabama","Alaska","Arizona","Arkansas","California","Colorado","Connecticut","Delaware","Florida","Georgia","Hawaii","Idaho","Illinois","Indiana","Iowa","Kansas","Kentucky","Louisiana","Maine","Maryland","Massachusetts","Michigan","Minnesota","Mississippi","Missouri","Montana","Nebraska","Nevada","New Hampshire","New Jersey","New Mexico","New York","North Dakota","North Carolina","Ohio","Oklahoma","Oregon","Pennsylvania","Rhode Island","South Carolina","South Dakota","Tennessee","Texas","Utah","Vermont","Virginia","Washington","West Virginia","Wisconsin","Wyoming"];
     // For Typeahead examples
     $scope.selectedName = "";
     $scope.names = ["Alabama","Alaska","Arizona","Arkansas","California","Colorado","Connecticut","Delaware","Florida","Georgia","Hawaii","Idaho","Illinois","Indiana","Iowa","Kansas","Kentucky","Louisiana","Maine","Maryland","Massachusetts","Michigan","Minnesota","Mississippi","Missouri","Montana","Nebraska","Nevada","New Hampshire","New Jersey","New Mexico","New York","North Dakota","North Carolina","Ohio","Oklahoma","Oregon","Pennsylvania","Rhode Island","South Carolina","South Dakota","Tennessee","Texas","Utah","Vermont","Virginia","Washington","West Virginia","Wisconsin","Wyoming"];
+
+    // For Accordion examples
+    $scope.panels = [
+      {
+        "title": "Collapsible Group Item #1",
+        "body": "Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch."
+      },
+      {
+        "title": "Collapsible Group Item #2",
+        "body": "Food truck fixie locavore, accusamus mcsweeney's marfa nulla single-origin coffee squid. Exercitation +1 labore velit, blog sartorial PBR leggings next level wes anderson artisan four loko farm-to-table craft beer twee."
+      },
+      {
+        "title": "Collapsible Group Item #3",
+        "body": "Etsy mixtape wayfarers, ethical wes anderson tofu before they sold out mcsweeney's organic lomo retro fanny pack lo-fi farm-to-table readymade."
+      },
+      {
+        "title": "Collapsible Group Item #4",
+        "body": "Reprehenderit butcher retro keffiyeh dreamcatcher synth. Cosby sweater eu banh mi, qui irure terry richardson ex squid."
+      }
+    ];
+    $scope.panels.activePanels = [];
+
   });
 
 })();
