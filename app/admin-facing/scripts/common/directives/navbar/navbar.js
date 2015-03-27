b6620d5e34f20de0eff799d8659753ecc4005590
Evan Black <evanmblack@gmail.com>
2015-03-27 17:08:39 -0500

    Implementing navbar directive with static content for now

---
+++ app/admin-facing/scripts/common/directives/navbar/navbar.js
@@ -0,0 +1,20 @@
+'use strict';
+
+angular.module('emmi.navbar', [])
+    .directive('emmiNavbar', function () {
+        return {
+            restrict: 'AE', // E = Element, A = Attribute, C = Class, M = Comment
+            templateUrl: '/admin-facing/partials/common/directives/navbar/navbar.tpl.html',
+            transclude: true,
+            scope: {
+
+            },
+            controller: function ($scope, $element, $attrs, $transclude) {
+
+            },
+            link: function (scope, element, attrs, controller) {
+
+            }
+        };
+    })
+;
