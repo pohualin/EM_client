15420ef36333bc1d7d2d62e46f52b5329b1cadfe
Evan Black <evanmblack@gmail.com>
2015-03-30 11:38:57 -0500

    Fix navbar template path

--- app/admin-facing/scripts/common/directives/navbar/navbar.js
+++ app/admin-facing/scripts/common/directives/navbar/navbar.js
@@ -4,7 +4,7 @@ angular.module('emmi.navbar', [])
     .directive('emmiNavbar', function () {
         return {
             restrict: 'AE', // E = Element, A = Attribute, C = Class, M = Comment
-            templateUrl: '/admin-facing/partials/common/directives/navbar/navbar.tpl.html',
+            templateUrl: 'admin-facing/partials/common/directives/navbar/navbar.tpl.html',
             transclude: true,
             controller: function ($scope, $element, $attrs, $transclude) {
 
