5bdbdfb1e31e4b14242e8ed860d38fe93b7f0528
Matt Fleming <mfleming@emmisolutions.com>
2015-03-31 10:32:17 -0500

    EM-895 A client user can select a location
    Complete

--- app/client-facing/schedule/program/add-program-ctrl.js
+++ app/client-facing/schedule/program/add-program-ctrl.js
@@ -8,26 +8,21 @@ angular.module('emmiManager')
             $controller('CommonPagination', {$scope: $scope});
             $controller('CommonSort', {$scope: $scope});
 
+            // initial loading
             var contentProperty = 'programs';
-
             $scope.specialties = null;
-            $scope.providers = null;
-
-            $scope.scheduledProgram = {
-                provider: '',
-                location: '',
-                program: '',
-                viewByDate: moment().add(30, 'days').format('YYYY-MM-DD')
-            };
-
-            AddProgramService.loadLocations($scope.team).then(function (locations){
+            $scope.scheduledProgram = AddProgramService.newScheduledProgram();
+            AddProgramService.loadLocations($scope.team).then(function (locations) {
                 $scope.locations = locations;
             });
-
-            AddProgramService.loadProviders($scope.team).then(function (providers){
+            AddProgramService.loadProviders($scope.team).then(function (providers) {
                 $scope.providers = providers;
             });
 
+            /**
+             * When the 'finish scheduling' button is clicked
+             * @param addProgramForm to save
+             */
             $scope.saveScheduledProgram = function (addProgramForm) {
                 $scope.addProgramFormSubmitted = true;
                 if ($scope.scheduledProgram.program && addProgramForm.$valid) {
@@ -38,6 +33,10 @@ angular.module('emmiManager')
                 }
             };
 
+            /**
+             * When show all is clicked
+             * @param show true means show all, false means show top 10 only
+             */
             $scope.showAllResults = function (show) {
                 $scope.showAll = show;
                 if (!show) {
@@ -45,6 +44,9 @@ angular.module('emmiManager')
                 }
             };
 
+            /**
+             * When there is a problem with the form
+             */
             $scope.showError = function () {
                 if (!$scope.errorAlert) {
                     $scope.errorAlert = $alert({
@@ -60,7 +62,14 @@ angular.module('emmiManager')
                 }
             };
 
-            var resetViewByDateField = function(form){
+            /**
+             * Necessary to carry the view by date across versions of the form.
+             * This shouldn't be necessary but with out it the view by date is
+             * blanked out when the program is selected (or 'edited').
+             *
+             * @param form on which to find the view by date
+             */
+            var resetViewByDateField = function (form) {
                 if (form) {
                     $timeout(function () {
                         form.viewByDate.$setViewValue(form.viewByDate.$viewValue);
@@ -68,6 +77,12 @@ angular.module('emmiManager')
                 }
             };
 
+            /**
+             * When a program is selected
+             *
+             * @param programResource to be selected
+             * @param form for date carrying function
+             */
             $scope.selectProgram = function (programResource, form) {
                 resetViewByDateField(form);
                 if ($scope.scheduledProgram.program) {
@@ -80,7 +95,48 @@ angular.module('emmiManager')
                 }
             };
 
-            $scope.reset = function (form) {
+            /**
+             * When a location selection is changed, we need to load the providers
+             * that are valid for that location
+             */
+            $scope.onLocationChange = function () {
+                $scope.loadingProviderLocation = true;
+                AddProgramService.loadProviders($scope.team, $scope.scheduledProgram.location).then(
+                    function (providers) {
+                        $scope.providers = providers;
+                        if (providers.length == 1) {
+                            $scope.scheduledProgram.provider = providers[0];
+                        }
+                    }).finally(function () {
+                        $scope.loadingProviderLocation = false;
+                    });
+            };
+
+            /**
+             * When a provider selection has changed, we need to load the
+             * list of possible locations for the selected provider
+             */
+            $scope.onProviderChange = function () {
+                // no location selected, refresh the list of possible locations
+                $scope.loadingProviderLocation = true;
+                AddProgramService.loadLocations($scope.team, $scope.scheduledProgram.provider).then(
+                    function (locations) {
+                        $scope.locations = locations;
+                        if (locations.length == 1) {
+                            $scope.scheduledProgram.location = locations[0];
+                        }
+                    }).finally(function () {
+                        $scope.loadingProviderLocation = false;
+                    });
+            };
+
+            /**
+             * Called when the program has already been selected and the
+             * user hits 'edit'
+             *
+             * @param form to be reset (and carry over the view by date)
+             */
+            $scope.editProgram = function (form) {
                 $scope.selectProgram(null, form);
                 $scope.addProgramFormSubmitted = false;
                 $scope.showAllResults(false);
--- app/client-facing/schedule/program/add-program-service.js
+++ app/client-facing/schedule/program/add-program-service.js
@@ -39,45 +39,64 @@ angular.module('emmiManager')
                 },
 
                 /**
-                 * Loads possible team locations
-                 *
-                 * @param teamResource to find the locations link
-                 * @returns {*}
+                 * Creates a new scheduled program object
+                 * @returns {{provider: string, location: string, program: string, viewByDate: *}}
                  */
-                loadLocations: function (teamResource) {
-                    var locations = [];
-                    return $http.get(UriTemplate.create(teamResource.link.locations).stringify())
-                        .then(function success(response) {
-                            var page = response.data;
-                            locations.push.apply(locations, page.content);
-                            if (page.link && page.link['page-next']) {
-                                $http.get(page.link['page-next']).then(function (response) {
-                                    success(response);
-                                });
-                            }
-                            return locations;
-                        });
+                newScheduledProgram: function () {
+                    return {
+                        provider: '',
+                        location: '',
+                        program: '',
+                        viewByDate: moment().add(30, 'days').format('YYYY-MM-DD')
+                    };
                 },
 
                 /**
-                 * Loads possible team providers
+                 * Loads all possible team locations
                  *
-                 * @param teamResource to find the providers link
+                 * @param teamResource to find the locations link
+                 * @param teamProviderResource to narrow the locations
                  * @returns {*}
                  */
-                loadProviders: function (teamResource) {
+                loadLocations: function (teamResource, teamProviderResource) {
+                    var locations = [];
+                    return $http.get(UriTemplate.create(teamResource.link.locations).stringify({
+                        teamProviderId: teamProviderResource && teamProviderResource.entity ?
+                            teamProviderResource.entity.id : ''
+                    })).then(function success(response) {
+                        var page = response.data;
+                        locations.push.apply(locations, page.content);
+                        if (page.link && page.link['page-next']) {
+                            $http.get(page.link['page-next']).then(function (response) {
+                                success(response);
+                            });
+                        }
+                        return locations;
+                    });
+                },
+
+                /**
+                 * Loads all possible team providers
+                 *
+                 * @param teamResource to find the providers link
+                 * @param teamLocationResource to narrow the providers list
+                 * @returns {*}
+                 */
+                loadProviders: function (teamResource, teamLocationResource) {
                     var providers = [];
-                    return $http.get(UriTemplate.create(teamResource.link.providers).stringify())
-                        .then(function success(response) {
-                            var page = response.data;
-                            providers.push.apply(providers, page.content);
-                            if (page.link && page.link['page-next']) {
-                                $http.get(page.link['page-next']).then(function (response) {
-                                    success(response);
-                                });
-                            }
-                            return providers;
-                        });
+                    return $http.get(UriTemplate.create(teamResource.link.providers).stringify({
+                        teamLocationId: teamLocationResource && teamLocationResource.entity ?
+                            teamLocationResource.entity.id : ''
+                    })).then(function success(response) {
+                        var page = response.data;
+                        providers.push.apply(providers, page.content);
+                        if (page.link && page.link['page-next']) {
+                            $http.get(page.link['page-next']).then(function (response) {
+                                success(response);
+                            });
+                        }
+                        return providers;
+                    });
                 }
             };
         }])
