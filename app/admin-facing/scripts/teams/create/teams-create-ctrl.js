56afbf9d11325abe3c4a340ec1636ea5606768c3
Matt Fleming <mfleming@emmisolutions.com>
2015-04-01 14:13:12 -0500

    EM-1043 Edit Team is broken

--- app/admin-facing/scripts/teams/create/teams-create-ctrl.js
+++ app/admin-facing/scripts/teams/create/teams-create-ctrl.js
@@ -4,49 +4,72 @@ angular.module('emmiManager')
 /**
  * Create a Single Team
  */
-    .controller('ClientTeamCreateCtrl', function ($scope, $http, $routeParams, Session, UriTemplate, CreateTeam, ViewTeam, $controller, clientResource, Client) {
+    .controller('ClientTeamCreateCtrl', ['$scope', 'CreateTeam', 'ViewTeam', '$controller', 'clientResource', 'Client', 'focus',
+        function ($scope, CreateTeam, ViewTeam, $controller, clientResource, Client, focus) {
 
-        $controller('TeamErrorController', {$scope: $scope});
+            $controller('TeamErrorController', {$scope: $scope});
 
-        $scope.teamToSave = CreateTeam.newTeam().entity;
+            $scope.teamToSave = CreateTeam.newTeam().entity;
 
-        $scope.teamClientResource = {
-            teamResource: {
-                entity: {}
-            },
-            clientResource: clientResource
-        };
+            $scope.teamClientResource = {
+                teamResource: {
+                    entity: {}
+                },
+                clientResource: clientResource
+            };
 
-        $controller('SalesForceCtrl', {$scope: $scope, team: $scope.teamToSave});
+            /**
+             * Necessary for the directives to communicate
+             * with each other
+             *
+             * @type {{now: boolean, click: boolean}}
+             */
+            $scope.checkingForDupes = {
+                now: false,
+                click: false
+            };
 
-        $scope.teamToSave.client = clientResource.entity;
-        $scope.url = clientResource.link.findByNormalizedName;
-        $scope.save = function (teamForm) {
-            var isValid = teamForm.$valid;
-            $scope.formSubmitted = true;
-            if (isValid && $scope.teamToSave.salesForceAccount) {
-                teamForm.$setPristine();
-                CreateTeam.insertTeams(clientResource, $scope.teamToSave).then(function (team) {
-                    ViewTeam.viewTeam(team.data.entity);
-                });
-                _paq.push(['trackEvent', 'Form Action', 'Team Create', 'Save']);
-            } else {
-                $scope.showError();
-                // Loop through the form's validation errors and log to Piwik
-                var formErrors = $scope.teamForm.$error;
-                for (var errorType in formErrors) {
-                    if (formErrors.hasOwnProperty(errorType)) {
-                        for (var i = 0; i < formErrors[errorType].length; i++) {
-                            _paq.push(['trackEvent', 'Validation Error', 'Team Create', formErrors[errorType][i].$name+' '+errorType]);
+            $scope.editMode = true;
+
+            $controller('SalesForceCtrl', {$scope: $scope, team: $scope.teamToSave});
+
+            if ($scope.cancelClickDereg){
+                $scope.cancelClickDereg();
+            }
+            $scope.cancelClickDereg = $scope.$on('$unsaved-form-cancel-clicked', function () {
+                $scope.editMode = true;
+                focus('teamName');
+            });
+
+            $scope.teamToSave.client = clientResource.entity;
+            $scope.url = clientResource.link.findByNormalizedName;
+            $scope.save = function (teamForm) {
+                var isValid = teamForm.$valid;
+                $scope.formSubmitted = true;
+                if (isValid && $scope.teamToSave.salesForceAccount) {
+                    teamForm.$setPristine();
+                    CreateTeam.insertTeams(clientResource, $scope.teamToSave).then(function (team) {
+                        ViewTeam.viewTeam(team.data.entity);
+                    });
+                    _paq.push(['trackEvent', 'Form Action', 'Team Create', 'Save']);
+                } else {
+                    $scope.showError();
+                    // Loop through the form's validation errors and log to Piwik
+                    var formErrors = $scope.teamForm.$error;
+                    for (var errorType in formErrors) {
+                        if (formErrors.hasOwnProperty(errorType)) {
+                            for (var i = 0; i < formErrors[errorType].length; i++) {
+                                _paq.push(['trackEvent', 'Validation Error', 'Team Create', formErrors[errorType][i].$name + ' ' + errorType]);
+                            }
                         }
                     }
                 }
-            }
-        };
+            };
 
-        $scope.cancel = function () {
-            Client.viewClient($scope.teamToSave.client);
-        };
+            $scope.cancel = function () {
+                $scope.editMode = false;
+                Client.viewClient($scope.teamToSave.client);
+            };
 
-    })
+        }])
 ;
