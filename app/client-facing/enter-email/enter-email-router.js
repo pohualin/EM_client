a879f9a752330863987e3510214e253409d17c25
Kevin Twery <ktwery@emmisolutions.com>
2015-04-07 12:25:22 -0500

    EM-734 -refactor changes

--- app/client-facing/enter-email/enter-email-router.js
+++ app/client-facing/enter-email/enter-email-router.js
@@ -1,31 +1,31 @@
-'use strict';
-
-angular.module('emmiManager')
-    .config(function ($routeProvider, USER_ROLES) {
-        // Routes
-        $routeProvider
-            .when('/addEmail', {
-                templateUrl: 'client-facing/enter-email/enter_email.html',
-                controller: 'enterEmail',
-                title: 'Enter Email',
-                access: {
-                    authorizedRoles: [USER_ROLES.all]
-                },
-                resolve: {
-                    account: ['AuthSharedService', function (AuthSharedService) {
-                        return AuthSharedService.currentUser();
-                    }],
-                    locationBeforeLogin: ['$rootScope', '$q',
-                        function ($rootScope, $q) {
-                            var deferred = $q.defer();
-                            if ($rootScope.locationBeforeLogin) {
-                                deferred.resolve($rootScope.locationBeforeLogin.path());
-                            } else {
-                                deferred.resolve('/');
-                            }
-                            return deferred.promise;
-                        }]
-                }
-            });
-    })
-;
+//'use strict';
+//
+//angular.module('emmiManager')
+//    .config(function ($routeProvider, USER_ROLES) {
+//        // Routes
+//        $routeProvider
+//            .when('/addEmail', {
+//                templateUrl: 'client-facing/enter-email/enter_email.html',
+//                controller: 'enterEmail',
+//                title: 'Enter Email',
+//                access: {
+//                    authorizedRoles: [USER_ROLES.all]
+//                },
+//                resolve: {
+//                    account: ['AuthSharedService', function (AuthSharedService) {
+//                        return AuthSharedService.currentUser();
+//                    }],
+//                    locationBeforeLogin: ['$rootScope', '$q',
+//                        function ($rootScope, $q) {
+//                            var deferred = $q.defer();
+//                            if ($rootScope.locationBeforeLogin) {
+//                                deferred.resolve($rootScope.locationBeforeLogin.path());
+//                            } else {
+//                                deferred.resolve('/');
+//                            }
+//                            return deferred.promise;
+//                        }]
+//                }
+//            });
+//    })
+//;
