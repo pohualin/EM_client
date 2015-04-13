0ed1b90cccfae38683dae3f36007f297a2c8598a
Evan Black <evanmblack@gmail.com>
2015-04-13 16:18:29 -0500

    Some navbar finalizing

--- app/admin-facing/scripts/common/directives/navbar/navbar.js
+++ app/admin-facing/scripts/common/directives/navbar/navbar.js
@@ -1,13 +1,14 @@
 'use strict';
 
 angular.module('emmi.navbar', [])
-    .directive('emmiNavbar', function ($window) {
+    .directive('emmiNavbar', function ($rootScope, $window, $location) {
         return {
             restrict: 'AE', // E = Element, A = Attribute, C = Class, M = Comment
             templateUrl: 'admin-facing/partials/common/directives/navbar/navbar.tpl.html',
             transclude: true,
             controller: function ($scope, $element, $attrs, $transclude) {
-
+                var filename = $window.location.pathname.substring($window.location.pathname.lastIndexOf('/')+1);
+                $scope.siteTool = filename.replace('.html', '');
             },
             link: function (scope, element, attrs, controller) {
                 var bodyEl = angular.element($window.document.body),
