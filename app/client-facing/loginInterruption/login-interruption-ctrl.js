0bb0cff1b675a2ce0daa3aef377c4ae974605692
Kevin Twery <ktwery@emmisolutions.com>
2015-04-23 16:24:58 -0500

    EM-1206

--- app/client-facing/loginInterruption/login-interruption-ctrl.js
+++ app/client-facing/loginInterruption/login-interruption-ctrl.js
@@ -1,8 +1,8 @@
 'use strict';
 
 angular.module('emmiManager')
-    .controller('loginInterruption', ['$scope', '$alert', '$location', 'locationBeforeLogin', 'account', 'userClientReqdResource', 'ValidationService',
-        function ($scope, $alert, $location, locationBeforeLogin, account, userClientReqdResource,ValidationService) {
+    .controller('loginInterruption', ['$scope', '$alert', '$location', 'locationBeforeLogin', 'account', 'userClientReqdResource',
+        function ($scope, $alert, $location, locationBeforeLogin, account, userClientReqdResource) {
             $scope.account = account;
             $scope.userClientReqdResource = userClientReqdResource;
             $scope.locationBeforeLogin = locationBeforeLogin;
