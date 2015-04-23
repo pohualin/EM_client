39c646b28cdd700e0ae89355e21b410b611cdea6
Kevin Twery <ktwery@emmisolutions.com>
2015-04-23 15:51:27 -0500

    EM-1206 -not showing proper page if user has email that is not validated

--- app/client-facing/loginInterruption/login-interruption-ctrl.js
+++ app/client-facing/loginInterruption/login-interruption-ctrl.js
@@ -1,11 +1,15 @@
 'use strict';
 
 angular.module('emmiManager')
-    .controller('loginInterruption', ['$scope', '$alert', '$location', 'locationBeforeLogin', 'account', 'userClientReqdResource',
-        function ($scope, $alert, $location, locationBeforeLogin, account, userClientReqdResource) {
+    .controller('loginInterruption', ['$scope', '$alert', '$location', 'locationBeforeLogin', 'account', 'userClientReqdResource', 'ValidationService',
+        function ($scope, $alert, $location, locationBeforeLogin, account, userClientReqdResource,ValidationService) {
             $scope.account = account;
             $scope.userClientReqdResource = userClientReqdResource;
             $scope.locationBeforeLogin = locationBeforeLogin;
+
+            ValidationService.get($scope.userClientReqdResource).then(function (accountWithOriginalEmail) {
+                $scope.account.originalUserClientEmail = accountWithOriginalEmail.originalUserClientEmail;
+            });
         }])
 ;
 
