45a91c5a61895eb86119446577a93d5e47a590f3
Amrita Chavan <achavan@emmisolutions.com>
2015-05-14 11:05:33 -0500

    EM-1049

--- app/client-facing/schedule/scheduled-program-factory.js
+++ app/client-facing/schedule/scheduled-program-factory.js
@@ -4,12 +4,7 @@ angular.module('emmiManager')
     .factory('ScheduledProgramFactory', [ function () {
         this.patient = {};
         this.scheduledProgram = null;
-        //provider: '',
-        //    location: '',
-        //    program: '',
-        //    viewByDate: moment().add(30, 'days').format('YYYY-MM-DD')
         this.valid = function (){
-            console.log(this);
           return this.patient && this.patient.id && this.scheduledProgram &&
               this.scheduledProgram.program && this.scheduledProgram.program.entity.id &&
               this.scheduledProgram.location.entity.id && this.scheduledProgram.provider.entity.id &&
