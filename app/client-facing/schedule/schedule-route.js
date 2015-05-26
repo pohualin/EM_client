6095f0de4465c33b7ab40c0f554eabd9644a8759
Amrita Chavan <achavan@emmisolutions.com>
2015-05-26 13:46:10 -0500

    code review

--- app/client-facing/schedule/schedule-route.js
+++ app/client-facing/schedule/schedule-route.js
@@ -8,25 +8,26 @@ angular.module('emmiManager')
            * Load the current team for the client, set up patient information if the route contains patient id
            */
             'team' : ['AuthSharedService', 'ScheduleService', '$q', '$route', 'ViewPatientService',
-                function (AuthSharedService, ScheduleService, $q, $route, ViewPatientService) {
-                    var deferred = $q.defer();
-                    AuthSharedService.currentUser().then(function (loggedInUser) {
-                        ScheduleService.loadTeam(loggedInUser.clientResource, $route.current.params.teamId)
-                            .then(function (response) {
-                                if ($route.current.params.patientId) {
-                                    ViewPatientService.loadPatient(response.data, $route.current.params.patientId).then(function (patientResponse) {
-                                        response.data.patient = patientResponse.data;
-                                        deferred.resolve(response.data);
-                                    });
-                                } else {
-                                    deferred.resolve(response.data);
-                                }
-                            }, function error() {
-                                deferred.reject();
-                            });
-                    });
-                    return deferred.promise;
-                }],
+              function (AuthSharedService, ScheduleService, $q, $route, ViewPatientService) {
+                  var deferred = $q.defer();
+                  AuthSharedService.currentUser().then(function (loggedInUser) {
+                      ScheduleService.loadTeam(loggedInUser.clientResource, $route.current.params.teamId)
+                          .then(function (response) {
+                              if ($route.current.params.patientId) {
+                                  ViewPatientService.loadPatient(response.data, $route.current.params.patientId).then(function (patientResponse) {
+                                      response.data.patient = patientResponse.data;
+                                      deferred.resolve(response.data);
+                                  });
+                              } else {
+                                  response.data.patient = {};
+                                  deferred.resolve(response.data);
+                              }
+                          }, function error() {
+                              deferred.reject();
+                          });
+                  });
+                  return deferred.promise;
+              }],
             /**
             * Load the client from the logged in user
             */
