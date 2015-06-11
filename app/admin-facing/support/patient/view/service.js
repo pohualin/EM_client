fea58f35111965c36cbb80a4fada7982f9fc33a1
Matt Fleming <mfleming@emmisolutions.com>
2015-06-11 13:54:49 -0500

    EM-1253 An Emmi User can search for a patient by first/last name across clients
    gulp error

--- app/admin-facing/support/patient/view/service.js
+++ app/admin-facing/support/patient/view/service.js
@@ -22,7 +22,7 @@
                             return response.data;
                         });
                     }
-                }
+                };
             }]);
 
 })(window.angular);
