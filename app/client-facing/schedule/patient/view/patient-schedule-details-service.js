c48ff5c4da1e3c4892872162c91f5e2439c8cd9c
Amrita Chavan <achavan@emmisolutions.com>
2015-06-16 10:44:30 -0500

    missing semicolon

--- app/client-facing/schedule/patient/view/patient-schedule-details-service.js
+++ app/client-facing/schedule/patient/view/patient-schedule-details-service.js
@@ -22,7 +22,7 @@ angular.module('emmiManager')
                     });
                     return deferred.promise;
                     }
-                }
+                };
             }
         ])
 ;
