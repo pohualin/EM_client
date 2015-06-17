d3cae4ba17939bc5c55d4a9a38300e74f3024c2c
Matt Fleming <7wjchnb836@snkmail.com>
2015-06-17 12:11:12 -0500

    EM-1382 An emmi user can view patient details from the patient search
    Everything working with the exception of phone/email configuration

--- app/admin-facing/support/patient/view/controller.js
+++ app/admin-facing/support/patient/view/controller.js
@@ -6,18 +6,70 @@
     /**
      * Search users across all clients
      */
-        .controller('PatientSupportViewController', ['$scope', 'patient', function ($scope, patientResource) {
-            $scope.patientResource = patientResource;
-            $scope.page.setTitle('Patient - ' +
-                patientResource.entity.firstName + ' ' +
-                patientResource.entity.lastName + ' | ClientManager');
+        .controller('PatientSupportViewController', ['$scope', '$alert', 'patient', 'PatientSupportViewService',
+            function ($scope, $alert, patientResource, PatientSupportViewService) {
 
-            $scope.optOutPreferences = [
-                {id: 1, typeKey: 'OPT_OUT_WHOLE_PATIENT'},
-                {id: 2, typeKey: 'OPT_OUT_PHONE'},
-                {id: 3, typeKey: 'OPT_OUT_EMAIL'}
-            ];
+                $scope.save = function (form) {
+                    $scope.formSubmitted = true;
+                    if (form.$valid) {
+                        $scope.saving = true;
+                        PatientSupportViewService.save($scope.patientResource).then(
+                            function ok(updatedPatientResource) {
+                                $scope.master = updatedPatientResource;
+                                $scope.cancel(form);
+                            }).finally(
+                            function () {
+                                $scope.saving = false;
+                            }
+                        );
+                    } else {
+                        $scope.showError();
+                    }
+                };
 
-        }]);
+                $scope.cancel = function (form) {
+                    $scope.formSubmitted = false;
+                    $scope.patientResource = angular.copy($scope.master);
+                    if (form) {
+                        form.$setPristine();
+                    }
+                };
+
+                $scope.patientHasChanged = function () {
+                    return !angular.equals($scope.master, $scope.patientResource);
+                };
+
+                $scope.showError = function () {
+                    if (!$scope.errorAlert) {
+                        $scope.errorAlert = $alert({
+                            title: ' ',
+                            content: 'Please correct the below information.',
+                            container: '#alerts-container',
+                            type: 'danger',
+                            show: true,
+                            duration: false,
+                            dismissable: false
+                        });
+                    } else {
+                        $scope.errorAlert.show();
+                    }
+                };
+
+                (function init() {
+                    $scope.master = patientResource;
+                    $scope.patientResource = angular.copy(patientResource);
+                    $scope.page.setTitle('Patient - ' +
+                        patientResource.entity.firstName + ' ' +
+                        patientResource.entity.lastName + ' | ClientManager');
+
+                    // load reference data for the screen
+                    PatientSupportViewService.loadReferenceData(patientResource).then(function (referenceData) {
+                        $scope.optOutPreferences = referenceData.optOutPreference;
+                    });
+                    $scope.cancel();
+                })();
+
+
+            }]);
 
 })(window.angular);
