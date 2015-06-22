56e8a0d547bee598b92a095170fcb1e5596a93fb
Amrita Chavan <achavan@emmisolutions.com>
2015-06-22 16:53:58 -0500

    EM714

--- app/admin-facing/scripts/teams/configuration/team-configuration-ctrl.js
+++ app/admin-facing/scripts/teams/configuration/team-configuration-ctrl.js
@@ -6,13 +6,13 @@ angular.module('emmiManager')
  *   Manage Team Level configuration a client
  */
     .controller('ClientTeamConfigurationCtrl', ['$scope', '$location', '$alert', 'focus', '$controller', 'teamResource', '$routeParams', 'API', 'ClientTeamConfigurationService',
-        function ($scope, $location, $alert, focus, $controller, teamResource,  $routeParams, API, ClientTeamConfigurationService) {
-    	    $scope.showTeamConfig = 'yes';
-    	    $scope.phoneClick = 'phone';
-    	    $scope.emailClick = 'email';
-    	    $scope.showEmailConfig = true;
-    	    $scope.showPhoneConfig = false;
-            $scope.page.setTitle('Team Configuration - '+ teamResource.entity.name +' | ClientManager');
+        function ($scope, $location, $alert, focus, $controller, teamResource, $routeParams, API, ClientTeamConfigurationService) {
+            $scope.showTeamConfig = 'yes';
+            $scope.phoneClick = 'phone';
+            $scope.emailClick = 'email';
+            $scope.showEmailConfig = true;
+            $scope.showPhoneConfig = false;
+            $scope.page.setTitle('Team Configuration - ' + teamResource.entity.name + ' | ClientManager');
             $scope.showSelfRegistrationConfig = false;
 
             /**
@@ -23,36 +23,35 @@ angular.module('emmiManager')
                 $location.path('/');
             };
 
-            $scope.onClick = function(configType){
-            	if(configType === 'phone'){
-            		$scope.showEmailConfig = false;
-            		$scope.showPhoneConfig = true;
+            $scope.onClick = function (configType) {
+                if (configType === 'phone') {
+                    $scope.showEmailConfig = false;
+                    $scope.showPhoneConfig = true;
                     $scope.showSelfRegistrationConfig = false;
-                }else{
-            		$scope.showEmailConfig = true;
-            	    $scope.showPhoneConfig = false;
+                } else {
+                    $scope.showEmailConfig = true;
+                    $scope.showPhoneConfig = false;
                     $scope.showSelfRegistrationConfig = false;
                 }
 
             };
 
-            $scope.showSelfRegSection = function(){
-                console.log('showSelfRegSection');
+            $scope.showSelfRegSection = function () {
                 $scope.showSelfRegistrationConfig = true;
                 $scope.showEmailConfig = false;
                 $scope.showPhoneConfig = false;
             };
 
-             /**
+            /**
              * init method called when page is loading
              */
             function init() {
-            	ClientTeamConfigurationService.setTeam(teamResource);
-            	$scope.client = teamResource.entity.client;
-            	$scope.team = teamResource;
+                ClientTeamConfigurationService.setTeam(teamResource);
+                $scope.client = teamResource.entity.client;
+                $scope.team = teamResource;
             }
 
             init();
 
-    }])
+        }])
 ;
