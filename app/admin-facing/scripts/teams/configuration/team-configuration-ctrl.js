487bd96ffb8b886959eb85b171fbe15370f466a0
Amrita Chavan <achavan@emmisolutions.com>
2015-06-23 18:03:45 -0500

    EM-714

--- app/admin-facing/scripts/teams/configuration/team-configuration-ctrl.js
+++ app/admin-facing/scripts/teams/configuration/team-configuration-ctrl.js
@@ -13,6 +13,8 @@ angular.module('emmiManager')
             $scope.showEmailConfig = true;
             $scope.showPhoneConfig = false;
             $scope.page.setTitle('Team Configuration - '+ teamResource.entity.name +' | ClientManager');
+            $scope.showSelfRegistrationConfig = false;
+
             /**
              * Called when cancel is clicked.. takes the original
              * objects and copies them back into the bound objects.
@@ -26,11 +28,19 @@ angular.module('emmiManager')
                 if(configType === 'phone'){
                     $scope.showEmailConfig = false;
                     $scope.showPhoneConfig = true;
+                    $scope.showSelfRegistrationConfig = false;
                 }else{
                     $scope.showEmailConfig = true;
                     $scope.showPhoneConfig = false;
+                    $scope.showSelfRegistrationConfig = false;
                 }
 
+            };
+
+            $scope.showSelfRegSection = function () {
+                $scope.showSelfRegistrationConfig = true;
+                $scope.showEmailConfig = false;
+                $scope.showPhoneConfig = false;
             };
 
             $scope.$on('showCardOutline', function (event, args) {
