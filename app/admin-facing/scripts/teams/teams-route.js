d221b1e7ceb8a65e783ec8d409554866b76a51ce
Paul Lin <plin@emmisolutions.com>
2015-07-07 10:26:54 -0500

    EM 1497 - Page flow

--- app/admin-facing/scripts/teams/teams-route.js
+++ app/admin-facing/scripts/teams/teams-route.js
@@ -22,6 +22,26 @@ angular.module('emmiManager')
             });
             return deferred.promise;
         }];
+        
+        var teamResource = ['AuthSharedService', 'Client', 'ViewTeam', '$route', '$q', function (AuthSharedService, Client, ViewTeam, $route, $q) {
+            var deferred = $q.defer();
+            AuthSharedService.currentUser().then(function () {
+                Client.selectClient($route.current.params.clientId).then(function (clientResource) {
+                    if (clientResource) {
+                        ViewTeam.selectTeam(clientResource.link.teamByTeamId, $route.current.params.clientId).then(function (teamResource) {
+                            if (teamResource) {
+                                deferred.resolve(teamResource);
+                            } else {
+                                deferred.reject();
+                            }
+                        });
+                    } else {
+                        deferred.reject();
+                    }
+                });
+            });
+            return deferred.promise;
+        }];
 
         // Routes
         $routeProvider
@@ -35,6 +55,19 @@ angular.module('emmiManager')
                 },
                 resolve: {
                     'clientResource': clientResource
+                }
+            })
+            .when('/clients/:clientId/teams/:teamId/new', {
+                templateUrl: 'admin-facing/partials/team/team_new.html',
+                controller: 'ClientTeamCreateCtrl',
+                title: 'New Team | ClientManager',
+                activeMenu: MENU.setup,
+                access: {
+                    authorizedRoles: USER_ROLES.all
+                },
+                resolve: {
+                    'clientResource': clientResource,
+                    'teamResource': teamResource
                 }
             })
             .when('/teams', {
