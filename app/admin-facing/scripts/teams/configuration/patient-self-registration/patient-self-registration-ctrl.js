db170604351800d48db6e957e182fda899a7b25c
Amrita Chavan <achavan@emmisolutions.com>
2015-07-11 14:14:07 -0500

    EM818

--- app/admin-facing/scripts/teams/configuration/patient-self-registration/patient-self-registration-ctrl.js
+++ app/admin-facing/scripts/teams/configuration/patient-self-registration/patient-self-registration-ctrl.js
@@ -1,10 +1,16 @@
 'use strict';
 
 angular.module('emmiManager')
-    .controller('PatientSelfRegController', ['$scope', 'PatientSelfRegService', function ($scope, PatientSelfRegService) {
+    .controller('PatientSelfRegController', ['$scope', 'PatientSelfRegService', '$translate', '$q', '$alert',
+        function ($scope, PatientSelfRegService, $translate, $q, $alert) {
 
     PatientSelfRegService.get($scope.team).then(function (response) {
         $scope.patientSelfRegConfig = response.entity;
+        if ($scope.patientSelfRegConfig === undefined){
+            $scope.patientSelfRegConfig = {};
+            $scope.patientSelfRegConfig.idLabelType = 'PATIENT_ID';
+                $scope.updatePatientIDLabelType($scope.patientSelfRegConfig.idLabelType);
+        }
     });
 
     $scope.continue = function (form) {
@@ -22,6 +28,16 @@ angular.module('emmiManager')
     $scope.create = function () {
         PatientSelfRegService.create($scope.team, $scope.patientSelfRegConfig).success(function (response) {
             $scope.patientSelfRegConfig = response.entity;
+            $alert({
+                title: ' ',
+                content: 'The patient self reg configuration has been saved successfully.',
+                container: 'body',
+                type: 'success',
+                placement: 'top',
+                show: true,
+                duration: 5,
+                dismissable: true
+            });
         })
         .finally(function () {
             $scope.whenSaving = false;
@@ -31,11 +47,35 @@ angular.module('emmiManager')
     $scope.update = function () {
         PatientSelfRegService.update($scope.team, $scope.patientSelfRegConfig).success(function (response) {
             $scope.patientSelfRegConfig = response.entity;
+            $alert({
+                title: ' ',
+                content: 'The team self reg configuration has been updated successfully.',
+                container: 'body',
+                type: 'success',
+                placement: 'top',
+                show: true,
+                duration: 5,
+                dismissable: true
+            });
         })
         .finally(function () {
             $scope.whenSaving = false;
         });
     };
-
+    $scope.updatePatientIDLabelType = function (idLabelType) {
+        if (idLabelType === 'OTHER_ID_LABEL') {
+            $scope.patientSelfRegConfig.patientIdLabelSpanish = '';
+            $scope.patientSelfRegConfig.patientIdLabelEnglish = '';
+        } else {
+            var promises = [];
+            promises.push($translate(idLabelType + '_SPANISH'));
+            promises.push($translate(idLabelType));
+            return $q.all(promises).then(function (response) {
+                $scope.patientSelfRegConfig.patientIdLabelSpanish = response[0];
+                $scope.patientSelfRegConfig.patientIdLabelEnglish = response [1];
+            });
+        }
+        ;
+    }
     }])
 ;
