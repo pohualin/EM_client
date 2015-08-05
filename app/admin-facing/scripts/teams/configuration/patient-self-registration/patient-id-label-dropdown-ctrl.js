3bc6f5eb148066601fa498cbf77b3958ed2d5010
Amrita Chavan <achavan@emmisolutions.com>
2015-08-05 15:08:50 -0500

    EM818

--- app/admin-facing/scripts/teams/configuration/patient-self-registration/patient-id-label-dropdown-ctrl.js
+++ app/admin-facing/scripts/teams/configuration/patient-self-registration/patient-id-label-dropdown-ctrl.js
@@ -4,6 +4,11 @@ angular.module('emmiManager')
     .controller('PatientIdLabelDropDownController', ['$scope', 'PatientSelfRegService', '$alert', 'PatientIdLabelDropDownService', '$controller',
         function ($scope, PatientSelfRegService, $alert, PatientIdLabelDropDownService, $controller) {
 
+            /**
+             * GET to find patient self reg config for a given team.
+             * Loads patient id label config if patient self reg config is present,
+             * else sets default value Patient Id in the dropdown for idLabelType
+             */
             PatientSelfRegService.get($scope.team).then(function (response) {
                 $scope.patientSelfRegConfig = response;
                 if ($scope.patientSelfRegConfig) {
@@ -20,11 +25,18 @@ angular.module('emmiManager')
                 }
             });
 
+            /**
+             * Sets default label to be Patient Id and grabs it's translations
+             */
             $scope.setDefaultIdLabel = function () {
                 $scope.patientSelfRegConfig.entity.patientIdLabelType = $scope.idLabelTypes[0];
                 $scope.updatePatientIDLabelTypeTranslations($scope.patientSelfRegConfig.entity.patientIdLabelType);
             };
 
+            /**
+             * creates a new patient id label config if one doesnt exist on the given patient self reg config, or
+             * updates a given patient id label config
+             */
             $scope.createOrUpdateIdLabelConfig = function () {
                 var listOfIdLabelConfigs = [];
                 listOfIdLabelConfigs.push($scope.idLabelConfigEnglish);
@@ -52,6 +64,10 @@ angular.module('emmiManager')
                 }
             };
 
+            /**
+             * Loads all patient id label configs for a given patient self reg config
+             * according to the language
+             */
             $scope.loadAllConfigs = function () {
                 PatientIdLabelDropDownService.getPatientIdLabelConfig($scope.patientSelfRegConfig).then(function (response) {
                     angular.forEach(response.content, function (config) {
@@ -68,6 +84,10 @@ angular.module('emmiManager')
                 });
             };
 
+            /**
+             * updates idLabelConfigEnglish and idLabelConfigSpanish for created/updated patient id self reg config
+             * @param config
+             */
             $scope.updateLabels = function (config) {
                 $scope.originalIdLabelConfig = $scope.patientSelfRegConfig.entity.patientIdLabelType;
                 angular.forEach($scope.languagesAvailable, function (language) {
