f898365d2fe4254db4e63a6986f1559a86ecaf3b
Amrita Chavan <achavan@emmisolutions.com>
2015-08-14 13:09:20 -0500

    EM714

--- app/admin-facing/scripts/teams/configuration/self-registration/self-registration-ctrl.js
+++ app/admin-facing/scripts/teams/configuration/self-registration/self-registration-ctrl.js
@@ -20,6 +20,11 @@ angular.module('emmiManager')
                 $scope.languagesAvailable = response;
             });
 
+            /**
+             * on click of continue on self-registration code section
+             * @param selfRegForm
+             * @param $event
+             */
             $scope.continue = function (selfRegForm, $event) {
                 $scope.selfRegFormSubmitted = true;
                 if (selfRegForm.$valid) {
@@ -33,6 +38,12 @@ angular.module('emmiManager')
                 }
             };
 
+            /**
+             * create a new self-registration code
+             * error scenario will invalidate the code field on form, and show a popover message
+             * @param selfRegForm
+             * @param $event
+             */
             $scope.create = function (selfRegForm, $event) {
                 SelfRegistrationService.create($scope.team, $scope.selfRegConfig).success(function (response) {
                     $scope.originalSelfRegConfig = angular.copy(response.entity);
@@ -49,29 +60,19 @@ angular.module('emmiManager')
                     });
                 })
                     .error(function (response, status) {
-                        if (status === 406) {
-                            selfRegForm.code.$setValidity('unique', false);
-                            if ($scope.uniquePopup) {
-                                $scope.uniquePopup.show();
-                            }
-                            else {
-                                $scope.conflictingConfig = angular.copy(response.entity);
-                                $scope.uniquePopup = $popover(angular.element($event.currentTarget), {
-                                    placement: 'top-right',
-                                    scope: $scope,
-                                    trigger: 'manual',
-                                    show: true,
-                                    contentTemplate: 'admin-facing/partials/team/configuration/self-registration/unique_self_reg_code_popover.tpl.html'
-                                });
-                            }
-                        }
-
+                        $scope.errorHandler(response, status, selfRegForm, $event);
                     })
                     .finally(function () {
                         $scope.whenSaving = false;
                     });
             };
 
+            /**
+             * update the existing self-registration code for a given team
+             * error scenario will invalidate the code field on form, and show a popover message
+             * @param selfRegForm
+             * @param $event
+             */
             $scope.update = function (selfRegForm, $event) {
                 SelfRegistrationService.update($scope.team, $scope.selfRegConfig).success(function (response) {
                     $scope.selfRegConfig = angular.copy(response.entity);
@@ -88,31 +89,41 @@ angular.module('emmiManager')
                     });
                 })
                     .error(function (response, status) {
-                        if (status === 406) {
-                            selfRegForm.code.$setValidity('unique', false);
-                            if ($scope.uniquePopup) {
-                                $scope.uniquePopup.show();
-                            }
-                            else {
-                                $scope.conflictingConfig = angular.copy(response.entity);
-                                $scope.uniquePopup = $popover(angular.element($event.currentTarget), {
-                                    placement: 'top-right',
-                                    scope: $scope,
-                                    trigger: 'manual',
-                                    show: true,
-                                    contentTemplate: 'admin-facing/partials/team/configuration/self-registration/unique_self_reg_code_popover.tpl.html'
-                                });
-                            }
-                        }
-
+                        $scope.errorHandler(response, status, selfRegForm, $event);
                     })
                     .finally(function () {
                         $scope.whenSaving = false;
                     });
-            }
-            ;
+            };
 
-            /**            * Reset all validity
+            /**
+             * handles the error case for create/update self-registration code
+             * @param response  the conflicting selfRegConfig that already has the code the user is trying to enter, duplicates not allowed
+             * @param status    error response status
+             * @param selfRegForm
+             * @param $event    the element for the popover
+             */
+            $scope.errorHandler = function (response, status, selfRegForm, $event) {
+                if (status === 406) {
+                    selfRegForm.code.$setValidity('unique', false);
+                    if ($scope.uniquePopup) {
+                        $scope.uniquePopup.show();
+                    }
+                    else {
+                        $scope.conflictingConfig = angular.copy(response.entity);
+                        $scope.uniquePopup = $popover(angular.element($event.currentTarget), {
+                            placement: 'top-right',
+                            scope: $scope,
+                            trigger: 'manual',
+                            show: true,
+                            contentTemplate: 'admin-facing/partials/team/configuration/self-registration/unique_self_reg_code_popover.tpl.html'
+                        });
+                    }
+                }
+            };
+
+            /**
+             * Reset all validity
              */
 
             $scope.resetValidity = function (form) {
@@ -123,6 +134,9 @@ angular.module('emmiManager')
                 }
             };
 
+            /**
+             * on click of cancel button for self-registration edit section
+             */
             $scope.cancel = function () {
                 $scope.selfRegFormSubmitted = false;
                 $scope.selfRegConfig = angular.copy($scope.originalSelfRegConfig);
