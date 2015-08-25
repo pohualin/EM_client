641e940e016ef60e00008936c917e0e2c35af527
Wendy Lee <wlee@emmisolutions.com>
2015-08-25 09:06:49 -0500

    EM-1520, An Emmi Admin can select a content subscription for a client

---
+++ app/admin-facing/scripts/configurations/program-content/content-subscription-ctrl.js
@@ -0,0 +1,190 @@
+'use strict';
+
+angular.module('emmiManager')
+
+/**
+ * Controller for ClientProgramContentConfiguration page
+ */
+    .controller('ClientProgramContentConfigurationController', ['$alert', '$scope', '$controller', 'clientResource', 'Client', 'ContentSubscriptionConfigurationService',
+        function ($alert, $scope, $controller, clientResource, Client, ContentSubscriptionConfigurationService) {
+        console.log('at the program content controller');  
+        $scope.faithBased = false;
+        $scope.sourceProgram = false;
+        $scope.primaryContentList = [];
+        $scope.sourceContentList = [];
+        $scope.showContentButton = false;
+        $scope.selectedSourceContent = [];
+        $scope.emmiEngagePlus = {};
+        $scope.noneContent = {name:'None', id:0};
+        $scope.deleteContentSubscription = false;
+        
+        console.log(clientResource);
+               
+        /**
+         * Cancel any changes
+         */
+        $scope.cancel = function (contentSubscriptionForm) {
+            contentSubscriptionForm.$setPristine();
+            $scope.contentSubscriptionFormSubmitted = false;
+            $scope.showButtons(false);
+        };
+        
+        /**
+         * Save content subscription configuration for the client
+         */
+        $scope.save = function(valid){
+            $scope.contentSubscriptionFormSubmitted = true;
+            if(valid){
+                $scope.whenSaving = true;
+                if($scope.deleteContentSubscription){
+                    ContentSubscriptionConfigurationService.deleteContent($scope.selectedContentSubscription).then(function(response){
+                    $alert({
+                        content: '<b>' + $scope.client.name + '</b> has been updated successfully.'
+                    });
+                    }).finally(function () {
+                        $scope.whenSaving = false;
+                    });
+                }
+                else{
+                      if($scope.selectedContentSubscription.entity.contentSubscription.id === 1288){
+                      $scope.selectedContentSubscription.entity.contentSubscription.id = 128;
+                      $scope.selectedContentSubscription.entity.contentSubscription.name = 'EmmiEngage';
+                      }
+                      ContentSubscriptionConfigurationService.saveOrUpdate($scope.selectedContentSubscription).then(function(response){
+                      $alert({
+                          content: '<b>' + $scope.client.name + '</b> has been updated successfully.'
+                      });
+                      }).finally(function () {
+                          $scope.whenSaving = false;
+                      });
+                }
+                
+            }else {
+                if (!$scope.formAlert) {
+                    $scope.formAlert = $alert({
+                        content: 'Please correct the below information.',
+                        container: '#message-container',
+                        type: 'danger',
+                        show: true,
+                        dismissable: false
+                    });
+                }
+            }
+
+             
+        };
+        
+        /**
+         * Show/hide cancel and save buttons
+         */
+        $scope.showButtons = function (showButton) {
+            return ($scope.showContentButton = showButton);
+         };
+        
+        $scope.createLists = function(contentList){
+            angular.forEach(contentList, function (aContent){
+                if(aContent.primarySbscrptn){
+                   $scope.primaryContentList.push(aContent);
+                }else if(aContent.sourceSbscrptn){
+                   $scope.sourceContentList.push(aContent);
+                }
+            });
+            
+            angular.forEach($scope.primaryContentList, function (pContent){
+                if(pContent.id === 128){
+                    angular.copy(pContent, $scope.emmiEngagePlus);
+                    $scope.emmiEngagePlus.id = 1288;
+                    $scope.emmiEngagePlus.name = 'EmmiEngage+';
+                 }
+            });
+            $scope.primaryContentList.splice(0, 0, $scope.emmiEngagePlus);
+            
+            $scope.primaryContentList.push($scope.noneContent);
+        };
+        
+        $scope.onChangePrimaryList = function(contentSubscriptionForm){
+            console.log($scope.selectedContentSubscription);
+            $scope.showContentButton = true;
+            if($scope.selectedContentSubscription.entity.contentSubscription.name === 'None'){
+                $scope.deleteContentSubscription = true;
+                $scope.faithBased = false;
+                $scope.sourceProgram = false;
+            }
+            else if(($scope.selectedContentSubscription.entity.contentSubscription.id === 1288) &&
+                    ($scope.selectedContentSubscription.entity.contentSubscription.name === 'EmmiEngage+')){
+                    console.log('Emmi Engage plus base');
+                    $scope.faithBased = true;
+                    $scope.sourceProgram = true;
+            }
+            else if($scope.selectedContentSubscription.entity.contentSubscription.id === 128){
+                console.log('Emmi Engage  base');
+                $scope.faithBased = true;
+                $scope.sourceProgram = false;
+            }
+            else{
+                $scope.faithBased = false;
+                $scope.sourceProgram = false;
+            }
+        };
+       
+       $scope.onChangeFaithBased = function(){
+           $scope.showContentButton = true;
+           
+       };
+       
+       $scope.onChangeSourceList = function(){
+           $scope.showContentButton = true;
+           if(angular.isDefined($scope.selectedSourceContent)){
+                $scope.selectedContentSubscription.entity.source = true;
+               
+           }
+           
+           
+       };
+       
+       // Will implement for story 1307
+       $scope.addAnotherSubscription = function(){
+           $scope.showContentButton = true;
+       };
+        /**
+         * init method called when page is loading
+         */
+       function init() {
+            $scope.client = Client.getClient().entity;
+            $scope.page.setTitle('Client Configurations - ' + $scope.client.name + ' | ClientManager');
+            
+            ContentSubscriptionConfigurationService.getContentSubscriptionList().then(function(response){
+                $scope.contentSubscriptions = response.content;
+                $scope.createLists(response.content);
+                 
+            });
+            ContentSubscriptionConfigurationService.getContentSubscriptionConfiguration().then(function (response) {
+                
+                $scope.originalContentSubscriptionConfiguration = response.content;
+                if(angular.isDefined(response.content)){
+                    $scope.selectedContentSubscription = $scope.originalContentSubscriptionConfiguration[0];
+                    if(($scope.selectedContentSubscription.entity.contentSubscription.id === 128) ||
+                        ($scope.selectedContentSubscription.entity.contentSubscription.id === 1288)){
+  
+                        $scope.faithBased = $scope.selectedContentSubscription.entity.faithBased;
+                        $scope.selectedSourceContent = $scope.selectedContentSubscription.entity.source;
+                        if($scope.selectedContentSubscription.entity.source){
+                            $scope.selectedContentSubscription.entity.contentSubscription.id = 1288;
+                            $scope.selectedContentSubscription.entity.contentSubscription.name = 'EmmiEngage+';
+                            $scope.sourceProgram = true;
+                        }
+                       
+                    }
+                   
+                }
+                else{
+                    $scope.selectedContentSubscription = ContentSubscriptionConfigurationService.createContentSubscriptionConfiguration();
+                }
+   
+            });
+ 
+       }
+
+        init();
+    }]);
+
---
+++ app/admin-facing/scripts/configurations/program-content/content-subscription-service.js
@@ -0,0 +1,83 @@
+'use strict';
+angular.module('emmiManager')
+
+/**
+ * This service is responsible CRUD operations for ContentSubscriptionConfiguration resources
+ */
+    .service('ContentSubscriptionConfigurationService', ['$filter', '$q', '$http', 'UriTemplate', 'CommonService', 'Client',
+        function ($filter, $q, $http, UriTemplate, CommonService, Client) {
+            return {
+                
+                /**
+                 * Call server to get a page of ContentSubscription list 
+                 */
+                getContentSubscriptionList: function () {
+                    console.log(Client.getClient().link);
+                    return $http.get(UriTemplate.create(Client.getClient().link.contentSubscriptions).stringify())
+                        .then(function (response) {
+                            CommonService.convertPageContentLinks(response.data);
+                            return response.data;
+                        });
+                },
+                
+                /**
+                 * Get ContentSubscriptionConfiguration by Client
+                 */
+                getContentSubscriptionConfiguration: function () {
+                    return $http.get(UriTemplate.create(Client.getClient().link.clientContentSubscriptionConfigurations).stringify())
+                        .then(function (response) {
+                            CommonService.convertPageContentLinks(response.data);
+                            return response.data;
+                        });
+                },
+                
+                /**
+                 * Return an empty ContentSubscriptionConfiguration
+                 */
+                createContentSubscriptionConfiguration: function () {
+                    return {
+                        entity: {
+                            contentSubscription: {name:'None', id:0},
+                            faithBased: false,
+                            source: false,
+                            sourceContentSubscription: null
+                        }
+                    };
+                },
+                
+                /**
+                 * Remove single ContentSubscriptionConfigurationService
+                 * @param contentSubscriptionConfiguration to remove
+                 * 
+                 */
+                remove: function(contentSubscriptionConfiguration){
+                    return $http.delete(UriTemplate.create(Client.getClient().link.clientContentSubscriptionConfigurations)
+                            .stringify()).then();
+                },
+                
+                /**
+                 * Save contentSubscriptionConfiguration
+                 */ 
+                saveOrUpdate: function(selectedContentSubscription){
+                    if(angular.isDefined(selectedContentSubscription.entity.id)){
+                        return $http.put(UriTemplate.create(Client.getClient().link.clientContentSubscriptionConfigurations).stringify(), 
+                                selectedContentSubscription.entity)
+                            .then(function (response) {
+                                CommonService.convertPageContentLinks(response.data);
+                                return response.data;
+                            });
+                    }
+                    else{
+                        return $http.post(UriTemplate.create(Client.getClient().link.clientContentSubscriptionConfigurations).stringify(), 
+                            selectedContentSubscription.entity)
+                        .then(function (response) {
+                            CommonService.convertPageContentLinks(response.data);
+                            return response.data;
+                        });
+                    }
+                }
+                
+            
+            };
+        }])
+;
