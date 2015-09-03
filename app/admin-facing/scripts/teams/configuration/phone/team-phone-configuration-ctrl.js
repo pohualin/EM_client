cf054153d299830973fa9e9349456142e1ccf7f9
Wade Courtney <wcourtney@emmisolutions.com>
2015-09-03 16:50:30 -0500

    EM-1493: Changing phone options to radio buttons instead of checkboxes.

--- app/admin-facing/scripts/teams/configuration/phone/team-phone-configuration-ctrl.js
+++ app/admin-facing/scripts/teams/configuration/phone/team-phone-configuration-ctrl.js
@@ -9,76 +9,90 @@ angular.module('emmiManager')
         function ($scope, $alert, teamResource, ClientTeamPhoneConfigurationService) {
 
             /**
-             * When the save button is clicked. Sends all updates
-             * to the back, then re-binds the form objects with the
-             * results
-             */
-            $scope.saveOrUpdatePhoneConfig = function (valid) {
-            	  if (valid) {
-                      $scope.whenSaving = true;
-            		  ClientTeamPhoneConfigurationService
-                          .saveOrUpdateTeamPhoneConfiguration($scope.team, $scope.phoneConfigs).then(function (response)
-                             {
-                        	  $scope.originalPhoneConfigs = response;
-                      		  $scope.phoneConfigs = angular.copy($scope.originalPhoneConfigs);
-                            	$alert({
-                                    content: '<strong>' + $scope.team.entity.name + '</strong> has been updated successfully.'
-                                });
-                             }).finally(function () {
-                              $scope.whenSaving = false;
-                          });
-            		  $scope.showPhoneButton = false;
-                  }
-            };
-
-
-            /**
-             * If user un-checks "Collect phone"
-             * "require phone" needs to automatically un-check.
-             */
-            $scope.onChangeCollect = function(){
-            	$scope.showPhoneButton  = true;
-            	if(!$scope.phoneConfigs.entity.collectPhone){
-                    $scope.phoneConfigs.entity.requirePhone = false;
-            	}
-           };
-
-           /**
-            * If user checks "Require phone"
-            * If yes, "Collect phone" needs to automatically check.
+            * When the save button is clicked. Sends all updates
+            * to the back, then re-binds the form objects with the
+            * results
             */
-           $scope.onChangeRequire = function(){
-        	   $scope.showPhoneButton  = true;
-           	   if($scope.phoneConfigs.entity.requirePhone){
-                   $scope.phoneConfigs.entity.collectPhone = true;
-           	   }
-          };
+            $scope.saveOrUpdatePhoneConfig = function (valid) {
+                if (valid) {
+                    $scope.whenSaving = true;
+                    ClientTeamPhoneConfigurationService
+                        .saveOrUpdateTeamPhoneConfiguration($scope.team, $scope.phoneConfigs).then(function (response) {
+                            $scope.originalPhoneConfigs = response;
+                            $scope.phoneConfigs = angular.copy($scope.originalPhoneConfigs);
 
-            /**
-             * Called when cancel is clicked.. takes the original
-             * objects and copies them back into the bound objects.
-             */
-            $scope.cancel = function () {
-            	$scope.phoneConfigs = angular.copy($scope.originalPhoneConfigs);
-            	$scope.showPhoneButton = false;
+                            $alert({
+                                content: '<strong>' + $scope.team.entity.name + '</strong> has been updated successfully.'
+                            });
+                        }).finally(function () {
+                            $scope.whenSaving = false;
+                    });
+
+                    $scope.showPhoneButton = false;
+                }
             };
 
+            /**
+            * Called when cancel is clicked.. takes the original
+            * objects and copies them back into the bound objects.
+            */
+            $scope.cancel = function() {
+                $scope.phoneConfigs = angular.copy($scope.originalPhoneConfigs);
+                $scope.showPhoneButton = false;
+                $scope.setPhoneOption();
+            };
+
+            $scope.update = function() {
+                if ($scope.phoneOptions.selected === $scope.phoneOptions[0]) {
+                    $scope.phoneConfigs.entity.collectPhone = true;
+                    $scope.phoneConfigs.entity.requirePhone = false;
+                } else if ($scope.phoneOptions.selected === $scope.phoneOptions[1]) {
+                    $scope.phoneConfigs.entity.collectPhone = true;
+                    $scope.phoneConfigs.entity.requirePhone = true;
+                } else if ($scope.phoneOptions.selected === $scope.phoneOptions[2]) {
+                    $scope.phoneConfigs.entity.collectPhone = false;
+                    $scope.phoneConfigs.entity.requirePhone = false;
+                }
+
+                $scope.showPhoneButton = true;
+            };
+
+            $scope.setPhoneOption = function() {
+                if ($scope.phoneConfigs.entity.collectPhone === true) {
+                    if ($scope.phoneConfigs.entity.requirePhone === true) {
+                        $scope.phoneOptions.selected = $scope.phoneOptions[1];
+                    } else {
+                        $scope.phoneOptions.selected = $scope.phoneOptions[0];
+                    }
+                } else {
+                    $scope.phoneOptions.selected = $scope.phoneOptions[2];
+                }
+            }
 
             /**
-             * init method called when page is loading
-             */
+            * init method called when page is loading
+            */
             function init() {
                 $scope.showPhoneButton = false;
 
+                $scope.phoneOptions = [
+                    { id: 'phoneExposed', displayText: 'Phone exposed', rank: 0 },
+                    { id: 'phoneExposedRequired', displayText: 'Phone exposed and required', rank: 1 },
+                    { id: 'dontCollectPhone', displayText: 'Don\'t collect phone number', rank: 2 }
+                ];
+                $scope.phoneOptions.selected = $scope.phoneOptions[0];
+
                 $scope.client = teamResource.entity.client;
-            	$scope.team = teamResource;
-            	ClientTeamPhoneConfigurationService.getTeamPhoneConfiguration($scope.team).then(function (response) {
-            		$scope.originalPhoneConfigs = response;
-            		$scope.phoneConfigs = angular.copy($scope.originalPhoneConfigs);
-               	});
+                $scope.team = teamResource;
+
+                ClientTeamPhoneConfigurationService.getTeamPhoneConfiguration($scope.team).then(function (response) {
+                    $scope.originalPhoneConfigs = response;
+                    $scope.phoneConfigs = angular.copy($scope.originalPhoneConfigs);
+
+                    $scope.setPhoneOption();
+                });
             }
 
             init();
-
     }])
 ;
