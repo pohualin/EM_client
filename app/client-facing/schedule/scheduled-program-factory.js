18d7729be698e0681d9784145341539011685686
Paul Lin <plin@emmisolutions.com>
2015-09-15 14:55:33 -0500

    EM 1624 - View by Date max 5 years from today

--- app/client-facing/schedule/scheduled-program-factory.js
+++ app/client-facing/schedule/scheduled-program-factory.js
@@ -4,7 +4,7 @@ angular.module('emmiManager')
 /**
  * Factory for maintaining patient and program and retrieve configuration for scheduling
  */
-    .factory('ScheduledProgramFactory',  ['$http', 'UriTemplate', function ($http, UriTemplate) {
+    .factory('ScheduledProgramFactory',  ['$http', '$q', 'UriTemplate', 'moment', function ($http, $q, UriTemplate, moment) {
         this.patient = {};
         this.scheduledProgram = null;
         this.selectedPrograms = null;
@@ -23,11 +23,21 @@ angular.module('emmiManager')
          * valid is set if patient is selected and program selected is complete (program has location, provider, viewByDate, program)
          */
         this.valid = function (scheduledProgram) {
+            
+            // Check viewByDate to be between today and 5 years from today
+            var validViewByDate = false;
+            if (scheduledProgram.viewByDate &&
+                    moment(scheduledProgram.viewByDate).isValid() &&
+                    !moment(scheduledProgram.viewByDate).isAfter(moment().startOf('day').add(5, 'year')) &&
+                    !moment(scheduledProgram.viewByDate).isBefore(moment().startOf('day'))) {
+                validViewByDate = true;
+            }
+            
             return this.patient && this.patient.id && this.teamSchedulingConfiguration &&
                 scheduledProgram.program && scheduledProgram.program.entity.id &&
                 (!this.teamSchedulingConfiguration.entity.useLocation || (this.teamSchedulingConfiguration.entity.useLocation && scheduledProgram.location.entity.id)) && 
                 (!this.teamSchedulingConfiguration.entity.useProvider || (this.teamSchedulingConfiguration.entity.useProvider && scheduledProgram.provider.entity.id)) && 
-                scheduledProgram.viewByDate;
+                validViewByDate;
         };
              
        /**
@@ -35,16 +45,18 @@ angular.module('emmiManager')
         */
        this.allValid = function () {
            var self = this;
+           var deferred = $q.defer();
            if (this.selectedPrograms) {
                this.selectedPrograms.forEach(function(program){
                    if (!self.valid(program)) {
-                       return false;
+                       deferred.resolve(false);
                    }
                });
-               return true;
+               deferred.resolve(true);
            } else {
-               return false;
+               deferred.resolve(false);
            }
+           return deferred.promise;
        };
        return this;
 
