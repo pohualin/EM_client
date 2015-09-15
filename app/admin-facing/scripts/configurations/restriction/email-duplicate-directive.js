46878be169cfae48ecf2218a8d5304e79e59ee2e
Wade Courtney <wcourtney@emmisolutions.com>
2015-09-15 17:02:46 -0500

    EM-1418

--- app/admin-facing/scripts/configurations/restriction/email-duplicate-directive.js
+++ app/admin-facing/scripts/configurations/restriction/email-duplicate-directive.js
@@ -16,9 +16,9 @@ angular.module('emmiManager')
                     var valid = true;
                     var emailConfigurations = EmailRestrictConfigurationsService.getEmailRestrictConfigurations();
 
-                    if (typeof emailConfigurations !== 'undefined') {
+                    if (typeof emailConfigurations !== 'undefined' && typeof newVal !== 'undefined') {
                         for (var i = 0, len = emailConfigurations.length; i < len; i++) {
-                            if (emailConfigurations[i].entity.emailEnding === newVal) {
+                            if (emailConfigurations[i].entity.emailEnding === newVal.toLowerCase()) {
                                 valid = false;
                                 break;
                             }
