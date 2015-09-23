09dae0d2262a9f7446eb2358577ccae2d3d0c988
Wendy Lee <wlee@emmisolutions.com>
2015-09-23 16:18:12 -0500

    EM-1700, merged conflicts

--- app/admin-facing/scripts/configurations/program-content/main-content-ctrl.js
+++ app/admin-facing/scripts/configurations/program-content/main-content-ctrl.js
@@ -7,83 +7,73 @@ angular.module('emmiManager')
  */
     .controller('MainContentConfigurationController', ['$alert', '$scope', '$controller', 'clientResource', 'Client', 'MainContentService', 'ContentSubscriptionConfigurationService',
         function ($alert, $scope, $controller, clientResource, Client, MainContentService, ContentSubscriptionConfigurationService) {
-       
+
     	// Store the original content subscription list
         $scope.primaryContentList = [];
         $scope.sourceContentList = [];
         $scope.latestPrimaryContentList = [];
-                        
+
         // Variables for the html to show or hide certain section
         $scope.faithBased = false;
         $scope.initialAddAnotherContentSubscription = false;
         $scope.noneSelected = false;
         $scope.showSelectList = false;
-        $scope.isEmmiEngagePlus = false;
-        
+        $scope.emmiEngagePlus = false;
+
         $scope.emmiEngagePlus = {};
         $scope.noneContent = {name:'None', id:0};
         $scope.selectedSourceContent = ContentSubscriptionConfigurationService.createContentSubscriptionConfiguration();
-        $scope.originalSourceContent = ContentSubscriptionConfigurationService.createContentSubscriptionConfiguration();
         $scope.selectedContentSubscription = ContentSubscriptionConfigurationService.createContentSubscriptionConfiguration();
         $scope.selectedContentList = [];
         $scope.showContentButton = false;
- 
-        
+
+
         /**
-         * Cancel button clicked
+         * Cancel any changes
          */
         $scope.cancel = function (contentSubscriptionForm) {
         	contentSubscriptionForm.$setPristine();
             $scope.contentSubscriptionFormSubmitted = false;
             $scope.initialAddAnotherContentSubscription = true;
+            $scope.selectedContentSubscription = ContentSubscriptionConfigurationService.createContentSubscriptionConfiguration();
             $scope.getClientContentList();
             $scope.$broadcast('event:remove-content-holder');
            	$scope.showButtons(false);
         };
-  
+
        /**
          * Show/hide cancel and save buttons
          */
         $scope.showButtons = function (showButton) {
          	return ($scope.showContentButton = showButton);
          };
-         
-         /**
-          * Reset selectedSourceContent
-          */
-         $scope.resetSelectedSourceContent = function (sourceContentToSave ) {
-        	 $scope.selectedSourceContent = sourceContentToSave;
-        	 $scope.originalSourceContent = sourceContentToSave;
-        	 
-          };
-        
-        
+
         /**
          * Reset initialAddAnotherContentSubscription
          */
         $scope.setInitialAddAnotherContentSubscription = function (newValue) {
         	$scope.initialAddAnotherContentSubscription = newValue;
         };
-        
+
         /**
          * Reset show select list or not
          */
         $scope.resetShowSelectList = function (newValue) {
         	 $scope.showSelectList = newValue;
         };
-        
+
         /**
          * show faith based check box or not
          */
         $scope.resetFaithBased = function (newValue) {
         	 $scope.faithBased = newValue;
         };
-        
+
         /*
          * update latest primary content list
          */
         $scope.updateLatestPrimaryContentList = function(aContent){
-        	$scope.latestPrimaryContentList = 
+        	$scope.latestPrimaryContentList =
         		ContentSubscriptionConfigurationService.filterLatestPrimaryContentList($scope.latestPrimaryContentList, aContent, $scope.selectedContentList.length);
         };
 
@@ -93,7 +83,7 @@ angular.module('emmiManager')
         $scope.noneSelectedForClient = function (newValue){
         	$scope.noneSelected = newValue;
         };
-          
+
         /**
           * Create the primaryContentSubscription and sourceContentSubscription
           * drop down list
@@ -115,20 +105,21 @@ angular.module('emmiManager')
             $scope.primaryContentList.splice(1, 0, $scope.emmiEngagePlus);
             $scope.primaryContentList.push($scope.noneContent);
             angular.copy($scope.primaryContentList, $scope.latestPrimaryContentList);
-            
+
          };
-         
+
+
          $scope.initialAddSubscription = function(){
            $scope.setInitialAddAnotherContentSubscription(false);
       	   $scope.resetShowSelectList(true);
       	   angular.forEach($scope.selectedContentList, function (newContentSubscription){
-      		  	 $scope.updateLatestPrimaryContentList(newContentSubscription);
+         	 $scope.updateLatestPrimaryContentList(newContentSubscription);
   	       });
-      	 
+
          };
-         
+
         /*
-         * Separate the content subscription list 
+         * Separate the content subscription list
          * that was selected from the database
          */
         $scope.separateSavedLists = function(contentList){
@@ -140,29 +131,27 @@ angular.module('emmiManager')
                     	angular.forEach(contentList, function (sourceContent){
                             if(sourceContent.entity.contentSubscription.sourceSubscription){
                             	angular.copy(sourceContent, $scope.selectedSourceContent);
-                            	$scope.originalSourceContent = sourceContent;
                             	aContent.entity.contentSubscription.name = 'EmmiEngage+';
                             	emmiEngage = true;
                               }
-                        });	
+                        });
                     }
                     angular.copy(aContent, $scope.selectedContentSubscription);
                     $scope.selectedContentList.push(aContent);
+                    $scope.addNewContentSubscription = true;
                  }
             });
                 angular.forEach($scope.selectedContentList, function (aContent){
             	$scope.updateLatestPrimaryContentList(aContent);
      	    });
-            $scope.isEmmiEngagePlus = emmiEngage;
+            $scope.emmiEngagePlus = emmiEngage;
        };
 
        $scope.getClientContentList = function(){
     	   $scope.selectedContentList = [];
-    	   $scope.latestPrimaryContentList = [];
-    	   angular.copy($scope.primaryContentList, $scope.latestPrimaryContentList);
     	   ContentSubscriptionConfigurationService.getClientContentSubscriptionConfiguration().then(function (response) {
                if(angular.isDefined(response.content)){
-            	       if(response.content.length > 0){
+                   if(response.content.length > 0){
                 	   $scope.showSelectList  = false;
                 	   $scope.initialAddAnotherContentSubscription = true;
                 	   $scope.separateSavedLists(response.content);
@@ -173,22 +162,22 @@ angular.module('emmiManager')
             	     $scope.initialAddAnotherContentSubscription = false;
                      $scope.faithBased = false;
                      $scope.showSelectList  = true;
-                     $scope.isEmmiEngagePlus = false;
-               }
+                     $scope.emmiEngagePlus = false;
+                     window.wen=$scope;
+                }
             });
         };
-       
+
        $scope.$on('selectedContentList', function () {
     	   $scope.getClientContentList();
        });
-       
- 
+
+
         /**
          * init method called when page is loading
          */
        function init() {
     	    $scope.client = Client.getClient().entity;
-            $scope.page.setTitle('Client Configurations - ' + $scope.client.name + ' | ClientManager');
             MainContentService.getContentSubscriptionList().then(function(response){
                 $scope.createLists(response.content);
             });
