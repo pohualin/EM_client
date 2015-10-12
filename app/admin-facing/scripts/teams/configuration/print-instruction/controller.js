ab94a466f4e9a0791542decf51ab6cdf5503bb2e
Matt Fleming <7wjchnb836@snkmail.com>
2015-10-12 16:18:36 -0500

    EM-1794 Email notification configs are missing
    * Fix for reset and multiple-save

--- app/admin-facing/scripts/teams/configuration/print-instruction/controller.js
+++ app/admin-facing/scripts/teams/configuration/print-instruction/controller.js
@@ -12,46 +12,48 @@ angular.module('emmiManager')
          * Save or update print instruction configuration
          * Reset form
          */
-        $scope.$on('event:save-or-update-print-instruction-configuration', function (event, args) {
+        $scope.$on('event:save-or-update-print-instruction-configuration', function () {
             TeamPrintInstructionConfigurationService
-                .saveOrUpdate(TeamEmailNotificationsAndPrintInstructionsControllerFactory.team, 
+                .saveOrUpdate(TeamEmailNotificationsAndPrintInstructionsControllerFactory.team,
                         $scope.teamPrintInstructionConfiguration).then(function(response){
                     $scope.initialCopy = response;
                     $scope.teamPrintInstructionConfiguration = angular.copy($scope.initialCopy);
-                    
+
                     $alert({
                         content: '<strong>' + TeamEmailNotificationsAndPrintInstructionsControllerFactory.team.entity.name + '</strong> has been updated successfully.'
                     });
                 }).finally(function () {
-                    TeamEmailNotificationsAndPrintInstructionsControllerFactory.reset();
+                    TeamEmailNotificationsAndPrintInstructionsControllerFactory.reset(
+                        TeamEmailNotificationsAndPrintInstructionsControllerFactory.team
+                    );
                 });
         });
-        
+
         /**
          * Reset print instruction configuration when cancel button is clicked
          */
         $scope.$on('event:reset-print-instruction-configuration', function () {
             $scope.teamPrintInstructionConfiguration = angular.copy($scope.initialCopy);
         });
-        
+
         $scope.$watch(function () {
             return TeamEmailNotificationsAndPrintInstructionsControllerFactory.formSubmitted;
         }, function (newVal) {
             $scope.formSubmitted = newVal;
         });
-        
+
         $scope.$watch(function () {
             return TeamEmailNotificationsAndPrintInstructionsControllerFactory.whenSaving;
         }, function (newVal) {
             $scope.whenSaving = newVal;
         });
-        
+
         function init() {
             TeamPrintInstructionConfigurationService.getTeamPrintInstructionConfiguration(TeamEmailNotificationsAndPrintInstructionsControllerFactory.team).then(function(response){
                 $scope.initialCopy = response;
                 $scope.teamPrintInstructionConfiguration = angular.copy($scope.initialCopy);
             });
         }
-        
+
         init();
 }]);
